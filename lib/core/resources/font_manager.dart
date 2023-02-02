27906f68cdce5f8d115975e380cd715ce8e0977f
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-02 20:13:36 +0200

    add FontConstants

---
+++ lib/core/resources/font_manager.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Style";
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.normal;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight bold = FontWeight.bold;
+}
+
+class FontSize {
+  static const double s0 = 0;
+  static const double s9 = 9.0;
+
+  static const double s10 = 10.0;
+  static const double s10_5 = 10.5;
+
+  static const double s11 = 11.0;
+  static const double s11_5 = 11.5;
+
+  static const double s12 = 12.0;
+  static const double s12_5 = 12.5;
+
+  static const double s13 = 13.0;
+  static const double s13_5 = 13.5;
+
+  static const double s14 = 14.0;
+  static const double s14_5 = 14.5;
+
+  static const double s15 = 15.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s19 = 19.0;
+
+  static const double s20 = 20.0;
+  static const double s21 = 21.0;
+  static const double s22 = 22.0;
+  static const double s23 = 23.0;
+  static const double s24 = 24.0;
+}
