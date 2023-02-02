6eb79a1b5f65a00b0607db167acb136204b336f5
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-02 20:15:42 +0200

    add some low files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:youtube/core/my_app.dart';
 
 import 'package:youtube/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
