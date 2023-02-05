387fc13f8e1ee795578af14bf424f90fdfdceab9
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-05 21:37:50 +0200

    refactoring HomePage

--- lib/presentation/pages/home/views/home_page.dart
+++ lib/presentation/pages/home/views/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:youtube/core/resources/color_manager.dart';
 import 'package:youtube/core/resources/styles_manager.dart';
-import 'package:youtube/core/widgets/custom_app_bar.dart';
+import 'package:youtube/presentation/pages/home/logic/home_page_logic.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,36 +11,72 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar.mainAppBar(),
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          const _VideosListView(),
-          Container(width: double.infinity,height: 50,color: ColorManager(context).black,)
-        ],
+      body: SafeArea(
+        child: CustomScrollView(slivers: [
+          const _CustomSliverAppBar(),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            childCount: 30,
+            (context, index) =>  _VideoItemList(index),
+          )),
+        ]),
       ),
     );
   }
 }
 
-class _VideosListView extends StatelessWidget {
-  const _VideosListView();
+class _CustomSliverAppBar extends StatelessWidget {
+  const _CustomSliverAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context, index) => Column(
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.bottomEnd,
-                  children: const [_Thumbnail(), _VideoTime()],
-                ),
-                const RSizedBox(height: 10),
-                const _VideoSubTitles(),
-              ],
-            ),
-        separatorBuilder: (context, index) => const RSizedBox(height: 15),
-        itemCount: 20);
+    return SliverAppBar(
+      floating: true,
+      leading: const Icon(Icons.yard_outlined, size: 40),
+      actionsIconTheme: const IconThemeData(size: 20),
+      actions: const [
+        Icon(Icons.search_rounded, size: 25),
+        RSizedBox(width: 20),
+        Icon(Icons.search_rounded, size: 25),
+        RSizedBox(width: 20),
+        Icon(Icons.search_rounded, size: 25),
+        RSizedBox(width: 20),
+        CircleAvatar(backgroundColor: ColorManager.teal, radius: 13),
+        RSizedBox(width: 20),
+      ],
+      bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50.h),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: ColorManager.green,
+            height: 50.h,
+          )),
+    );
+  }
+}
+
+class _VideoItemList extends StatelessWidget {
+  const _VideoItemList(this.index);
+final int index;
+  @override
+  Widget build(BuildContext context) {
+    final miniVideoViewLogic = Get.find<MiniVideoViewLogic>(tag: "1");
+
+    return InkWell(
+      onTap: () {
+        miniVideoViewLogic.videoIndex.value=index;
+      },
+      child: Column(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: const [_Thumbnail(), _VideoTime()],
+          ),
+          const RSizedBox(height: 10),
+          const _VideoSubTitles(),
+        ],
+      ),
+    );
   }
 }
 
@@ -105,7 +142,7 @@ class _VideoTime extends StatelessWidget {
     return Padding(
       padding: REdgeInsets.all(8.0),
       child: Container(
-        padding: REdgeInsets.symmetric(horizontal: 2, vertical: 1),
+        padding: REdgeInsets.symmetric(horizontal: 2, vertical: 1.5),
         decoration: BoxDecoration(
             color: ColorManager(context).black87,
             borderRadius: BorderRadius.circular(2)),
