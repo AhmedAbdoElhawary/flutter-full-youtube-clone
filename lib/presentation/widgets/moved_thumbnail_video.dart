54735bb9772f9e439c805e0c7a4c759f68336c91
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-08 20:57:23 +0200

    change the name ThumbnailVideoItem to MovedThumbnailVideo

--- lib/presentation/widgets/moved_thumbnail_video.dart
+++ lib/presentation/widgets/moved_thumbnail_video.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:youtube/core/resources/assets_manager.dart';
 import 'package:youtube/presentation/widgets/circular_profile_image.dart';
+import 'package:youtube/presentation/widgets/small_thumbnail_video.dart';
 
 import '../../core/resources/color_manager.dart';
 import '../../core/resources/styles_manager.dart';
 import '../pages/home/logic/home_page_logic.dart';
 
-class ThumbnailVideoItem extends StatelessWidget {
-  const ThumbnailVideoItem(this.index, {super.key});
+class MovedThumbnailVideo extends StatelessWidget {
+  const MovedThumbnailVideo(this.index, {super.key});
   final int index;
   @override
   Widget build(BuildContext context) {
@@ -16,13 +19,13 @@ class ThumbnailVideoItem extends StatelessWidget {
 
     return InkWell(
       onTap: () {
-        miniVideoViewLogic.videoIndex.value=index;
+        miniVideoViewLogic.videoIndex.value = index;
       },
       child: Column(
         children: [
           Stack(
             alignment: AlignmentDirectional.bottomEnd,
-            children:  [ThumbnailVideo(index),const _VideoTime()],
+            children: [ThumbnailVideo(index), const _VideoTime()],
           ),
           const RSizedBox(height: 10),
           const _VideoSubTitles(),
@@ -48,7 +51,7 @@ class _VideoSubTitles extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Flutter youtube fully clone With firebase "*2,
+                Text("Flutter youtube fully clone With firebase " * 2,
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                     style: getMediumStyle(
@@ -63,7 +66,7 @@ class _VideoSubTitles extends StatelessWidget {
             ),
           ),
           const RSizedBox(width: 10),
-          const Icon(Icons.circle, size: 10),
+          SvgPicture.asset(IconsAssets.menuPointsVerticalIcon),
         ],
       ),
     );
@@ -85,24 +88,9 @@ class _VideoTime extends StatelessWidget {
         child: Text(
           "18:14",
           style:
-          getMediumStyle(color: ColorManager(context).white, fontSize: 12),
+              getMediumStyle(color: ColorManager(context).white, fontSize: 12),
         ),
       ),
-    );
-  }
-}
-
-class ThumbnailVideo extends StatelessWidget {
-  const ThumbnailVideo(this.index,{Key? key}) : super(key: key);
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 185.h,
-      color: ColorManager.yellow,
-        child: Center(child: Text("$index")),
     );
   }
 }
