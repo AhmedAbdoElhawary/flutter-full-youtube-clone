d7d5388d835cb2ffac51aef1b40fd3648699d39c
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-11 13:40:24 +0200

    add MultiBlocProvider to MpApp

--- lib/core/widgets/multi_bloc_provider.dart
+++ lib/core/widgets/multi_bloc_provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:youtube/core/utility/injector.dart';
+import 'package:youtube/presentation/cubit/videos_details/videos_details_cubit.dart';
 
 class MultiBlocs extends StatelessWidget {
   final Widget materialApp;
@@ -7,6 +10,10 @@ class MultiBlocs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return materialApp;
+    return MultiBlocProvider(providers: [
+      BlocProvider<VideosDetailsCubit>(
+        create: (context) => injector<VideosDetailsCubit>(),
+      ),
+    ], child: materialApp);
   }
 }
