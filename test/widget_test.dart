caadd3f835d63647546b44db0170c305999e657d
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-11 13:38:38 +0200

    refactoring the code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:youtube/core/my_app.dart';
 
-import 'package:youtube/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
