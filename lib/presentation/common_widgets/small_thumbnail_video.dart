e97de8d58a0da5d0c9220df58f5513bd69a6052c
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-11 13:36:58 +0200

    create ThumbnailVideo

---
+++ lib/presentation/common_widgets/small_thumbnail_video.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:youtube/data/models/video_details/video_details.dart';
+
+import '../../core/resources/color_manager.dart';
+
+class ThumbnailVideo extends StatelessWidget {
+  const ThumbnailVideo(this.thumbnailsVideo,
+      {this.width = double.infinity, this.height = 185, Key? key})
+      : super(key: key);
+  final Thumbnails? thumbnailsVideo;
+  final double height;
+  final double width;
+  @override
+  Widget build(BuildContext context) {
+    String url = thumbnailsVideo!.max?.url ?? "";
+    return Container(
+      width: width.w,
+      height: height.h,
+      color: ColorManager(context).grey1,
+      child: url.isNotEmpty ? Image.network(url, fit: BoxFit.cover) : null,
+    );
+  }
+}
