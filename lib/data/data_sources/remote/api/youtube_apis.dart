f01e6e1b8eb03d3167b205631a3aa16e218b3a9b
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-11 22:38:44 +0200

    add some dependencies

--- lib/data/data_sources/remote/api/youtube_apis.dart
+++ lib/data/data_sources/remote/api/youtube_apis.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:youtube/core/utility/constants.dart';
-import 'package:youtube/core/utility/private_keys.dart';
+import 'package:youtube/core/utility/private_key.dart';
 import 'package:youtube/data/models/video_details/video_details.dart';
 part 'youtube_apis.g.dart';
 
