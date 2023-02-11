f16c9d083a3e33c4f259ff330071f6228afd4e0e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-11 13:41:12 +0200

    initialize dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:youtube/core/my_app.dart';
+import 'package:youtube/core/utility/injector.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-
+  await initializeDependencies();
   runApp(const MyApp());
 }
