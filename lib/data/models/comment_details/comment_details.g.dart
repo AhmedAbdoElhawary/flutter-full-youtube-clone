cdbb7ab2f2f9e8bd94b2fc5f7dbbd438dc878086
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-14 00:03:43 +0200

    solve comment snippet issues

--- lib/data/models/comment_details/comment_details.g.dart
+++ lib/data/models/comment_details/comment_details.g.dart
@@ -24,37 +24,8 @@ CommentDetailsItem _$CommentDetailsItemFromJson(Map<String, dynamic> json) =>
       id: json['id'] as String?,
       snippet: json['snippet'] == null
           ? null
-          : CommentSnippet.fromJson(json['snippet'] as Map<String, dynamic>),
-    );
-
-CommentSnippet _$CommentSnippetFromJson(Map<String, dynamic> json) =>
-    CommentSnippet(
-      topLevelComment: json['topLevelComment'] == null
-          ? null
-          : TopLevelComment.fromJson(
-              json['topLevelComment'] as Map<String, dynamic>),
-      canReply: json['canReply'] as bool?,
-      totalReplyCount: json['totalReplyCount'] as int?,
-      isPublic: json['isPublic'] as bool?,
-      videoId: json['videoId'] as String?,
-      textDisplay: json['textDisplay'] as String?,
-      textOriginal: json['textOriginal'] as String?,
-      authorDisplayName: json['authorDisplayName'] as String?,
-      authorProfileImageUrl: json['authorProfileImageUrl'] as String?,
-      authorChannelUrl: json['authorChannelUrl'] as String?,
-      authorChannelId: json['authorChannelId'] == null
-          ? null
-          : AuthorChannelId.fromJson(
-              json['authorChannelId'] as Map<String, dynamic>),
-      canRate: json['canRate'] as bool?,
-      viewerRating: json['viewerRating'] as String?,
-      likeCount: json['likeCount'] as int?,
-      publishedAt: json['publishedAt'] == null
-          ? null
-          : DateTime.parse(json['publishedAt'] as String),
-      updatedAt: json['updatedAt'] == null
-          ? null
-          : DateTime.parse(json['updatedAt'] as String),
+          : MainCommentSnippet.fromJson(
+              json['snippet'] as Map<String, dynamic>),
     );
 
 TopLevelComment _$TopLevelCommentFromJson(Map<String, dynamic> json) =>
@@ -62,5 +33,17 @@ TopLevelComment _$TopLevelCommentFromJson(Map<String, dynamic> json) =>
       id: json['id'] as String?,
       snippet: json['snippet'] == null
           ? null
-          : CommentSnippet.fromJson(json['snippet'] as Map<String, dynamic>),
+          : SubCommentSnippet.fromJson(json['snippet'] as Map<String, dynamic>),
+    );
+
+MainCommentSnippet _$MainCommentSnippetFromJson(Map<String, dynamic> json) =>
+    MainCommentSnippet(
+      videoId: json['videoId'] as String?,
+      topLevelComment: json['topLevelComment'] == null
+          ? null
+          : TopLevelComment.fromJson(
+              json['topLevelComment'] as Map<String, dynamic>),
+      canReply: json['canReply'] as bool?,
+      totalReplyCount: json['totalReplyCount'] as int?,
+      isPublic: json['isPublic'] as bool?,
     );
