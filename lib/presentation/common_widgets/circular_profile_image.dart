17d09e0afafd604a2e4ea1c6aab1ea76322a4110
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 18:20:15 +0200

    feed CircularProfileImage with actual data

--- lib/presentation/common_widgets/circular_profile_image.dart
+++ lib/presentation/common_widgets/circular_profile_image.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube/config/routes/route_app.dart';
+import 'package:youtube/data/models/channel_details/channel_details.dart';
 
 import '../../core/resources/color_manager.dart';
 import '../pages/channel_profile/user_profile_page.dart';
@@ -8,11 +9,15 @@ import '../pages/channel_profile/user_profile_page.dart';
 class CircularProfileImage extends StatelessWidget {
   final bool enableTapping;
   final double radius;
-  const CircularProfileImage({this.enableTapping = true,this.radius=18, Key? key})
+  final ChannelSubDetails? channelSubDetails;
+  const CircularProfileImage(
+      {this.channelSubDetails,this.enableTapping = true, this.radius = 18, Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String imageUrl =
+        channelSubDetails?.items?[0]?.snippet?.thumbnails?.high?.url ?? "";
     return GestureDetector(
       onTap: !enableTapping
           ? null
@@ -22,6 +27,7 @@ class CircularProfileImage extends StatelessWidget {
       child: CircleAvatar(
         radius: radius.r,
         backgroundColor: ColorManager(context).grey,
+        backgroundImage: imageUrl.isNotEmpty ? NetworkImage(imageUrl) : null,
       ),
     );
   }
