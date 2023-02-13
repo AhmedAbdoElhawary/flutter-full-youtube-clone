d1c55397a35a0bee82ca3e5f90f5a2e7e02c338b
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-14 00:04:41 +0200

    refactoring the code

--- lib/domain/repositories/channel_details_repository.dart
+++ lib/domain/repositories/channel_details_repository.dart
@@ -1,7 +1,10 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/data/models/channel_details/channel_details.dart';
+import 'package:youtube/data/models/video_details/video_details.dart';
 
 abstract class ChannelDetailsRepository {
-  Future<ApiResult<ChannelSubDetails>> getSubChannelDetails(
+  Future<ApiResult<ChannelSubDetails>> getSubSingleChannelDetails(
       {required String channelId});
+  Future<VideosDetails> getSubChannelsDetails(
+      {required  VideosDetails videosDetails});
 }
