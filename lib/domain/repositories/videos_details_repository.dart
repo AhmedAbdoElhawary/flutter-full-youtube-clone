d2fd5acac2b03d256d969d0d794b7e4847431fda
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 18:18:20 +0200

    change VideosDetails to VideoDetails

--- lib/domain/repositories/videos_details_repository.dart
+++ lib/domain/repositories/videos_details_repository.dart
@@ -2,6 +2,6 @@ import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/data/models/video_details/video_details.dart';
 
 abstract class VideoDetailsRepository {
-  Future<ApiResult<VideoDetails>> getMostPopularVideos();
-  Future<ApiResult<VideoDetails>> getVideoDetails({required String videoId});
+  Future<ApiResult<VideosDetails>> getMostPopularVideos();
+  Future<ApiResult<VideosDetails>> getVideoDetails({required String videoId});
 }
