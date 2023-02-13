f11d1902bec4fa5e604e4002f467df96f81918f3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 21:11:05 +0200

    create _$PageInfoFromJson

---
+++ lib/data/models/common/page_info/page_info.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'page_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PageInfo _$PageInfoFromJson(Map<String, dynamic> json) => PageInfo(
+      totalResults: json['totalResults'] as int?,
+      resultsPerPage: json['resultsPerPage'] as int?,
+    );
