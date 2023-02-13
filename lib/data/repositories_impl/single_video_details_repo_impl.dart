5a26f3867a6f535aa20ad50dbd190cc9f763e6c9
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-14 00:02:52 +0200

    build body of SingleVideosDetailsRepoImpl

--- lib/data/repositories_impl/single_video_details_repo_impl.dart
+++ lib/data/repositories_impl/single_video_details_repo_impl.dart
@@ -2,12 +2,17 @@ import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/functions/network_exceptions.dart';
 import 'package:youtube/data/data_sources/remote/api/single_video/single_video_apis.dart';
 import 'package:youtube/data/models/comment_details/comment_details.dart';
+import 'package:youtube/data/models/reply_details/reply_details.dart';
 import 'package:youtube/data/models/video_details/video_details.dart';
+import 'package:youtube/domain/repositories/channel_details_repository.dart';
 import 'package:youtube/domain/repositories/single_video_details_repository.dart';
 
 class SingleVideosDetailsRepoImpl implements SingleVideoDetailsRepository {
   final SingleVideosAPIs _singleVideosAPIs;
-  SingleVideosDetailsRepoImpl(this._singleVideosAPIs);
+  final ChannelDetailsRepository _channelDetailsRepository;
+
+  SingleVideosDetailsRepoImpl(
+      this._singleVideosAPIs, this._channelDetailsRepository);
 
   @override
   Future<ApiResult<VideosDetails>> getVideoDetails(
@@ -15,14 +20,20 @@ class SingleVideosDetailsRepoImpl implements SingleVideoDetailsRepository {
     try {
       VideosDetails videos =
           await _singleVideosAPIs.getVideoDetails(videoId: videoId);
-      return ApiResult.success(videos);
+
+      VideosDetails videosWithSubChannelDetails =
+          await _channelDetailsRepository.getSubChannelsDetails(
+              videosDetails: videos);
+
+      return ApiResult.success(videosWithSubChannelDetails);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
     }
   }
 
   @override
-  Future<ApiResult<CommentDetails>> getAllComments({required String videoId})async {
+  Future<ApiResult<CommentDetails>> getAllComments(
+      {required String videoId}) async {
     try {
       CommentDetails commentDetails =
           await _singleVideosAPIs.getAllComments(videoId: videoId);
@@ -33,7 +44,8 @@ class SingleVideosDetailsRepoImpl implements SingleVideoDetailsRepository {
   }
 
   @override
-  Future<ApiResult<CommentDetails>> getFirstComment({required String videoId}) async{
+  Future<ApiResult<CommentDetails>> getFirstComment(
+      {required String videoId}) async {
     try {
       CommentDetails firstComment =
           await _singleVideosAPIs.getFirstComment(videoId: videoId);
@@ -44,10 +56,11 @@ class SingleVideosDetailsRepoImpl implements SingleVideoDetailsRepository {
   }
 
   @override
-  Future<ApiResult<CommentDetails>> getRepliesForThisComment({required String commentId})async {
+  Future<ApiResult<ReplyDetails>> getRepliesForThisComment(
+      {required String commentId}) async {
     try {
-      CommentDetails replies =
-          await _singleVideosAPIs.getRepliesForThisComment(commentId: commentId);
+      ReplyDetails replies = await _singleVideosAPIs.getRepliesForThisComment(
+          commentId: commentId);
       return ApiResult.success(replies);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
--- lib/domain/repositories/single_video_details_repository.dart
+++ lib/domain/repositories/single_video_details_repository.dart
@@ -1,11 +1,12 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/data/models/comment_details/comment_details.dart';
+import 'package:youtube/data/models/reply_details/reply_details.dart';
 import 'package:youtube/data/models/video_details/video_details.dart';
 
 abstract class SingleVideoDetailsRepository {
   Future<ApiResult<VideosDetails>> getVideoDetails({required String videoId});
   Future<ApiResult<CommentDetails>> getAllComments({required String videoId});
   Future<ApiResult<CommentDetails>> getFirstComment({required String videoId});
-  Future<ApiResult<CommentDetails>> getRepliesForThisComment(
+  Future<ApiResult<ReplyDetails>> getRepliesForThisComment(
       {required String commentId});
 }
