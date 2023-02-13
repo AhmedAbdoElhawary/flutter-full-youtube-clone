1b302570d39fcb79092b52e954cfc72c4f83cf1f
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-14 00:04:39 +0200

    refactoring the code

--- lib/data/repositories_impl/channel_details_repo_impl.dart
+++ lib/data/repositories_impl/channel_details_repo_impl.dart
@@ -2,6 +2,7 @@ import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/functions/network_exceptions.dart';
 import 'package:youtube/data/data_sources/remote/api/channel/channel_apis.dart';
 import 'package:youtube/data/models/channel_details/channel_details.dart';
+import 'package:youtube/data/models/video_details/video_details.dart';
 import 'package:youtube/domain/repositories/channel_details_repository.dart';
 
 class ChannelDetailsRepoImpl implements ChannelDetailsRepository {
@@ -9,14 +10,31 @@ class ChannelDetailsRepoImpl implements ChannelDetailsRepository {
   ChannelDetailsRepoImpl(this._channelAPIs);
 
   @override
-  Future<ApiResult<ChannelSubDetails>> getSubChannelDetails(
+  Future<ApiResult<ChannelSubDetails>> getSubSingleChannelDetails(
       {required String channelId}) async {
     try {
       ChannelSubDetails channelSubDetails =
-      await _channelAPIs.getSubChannelInfo(channelId: channelId);
+          await _channelAPIs.getSubChannelInfo(channelId: channelId);
       return ApiResult.success(channelSubDetails);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
+    }
+  }
+
+  @override
+  Future<VideosDetails> getSubChannelsDetails(
+      {required VideosDetails videosDetails}) async {
+    try {
+      for (final item
+          in videosDetails.videoDetailsItem ?? <VideoDetailsItem>[]) {
+        String channelId = item.snippet?.channelId ?? "";
+        ChannelSubDetails subChannelDetails =
+            await _channelAPIs.getSubChannelInfo(channelId: channelId);
+        item.snippet?.channelSubDetails = subChannelDetails;
+      }
+      return videosDetails;
+    } catch (e) {
+      return Future.error(e);
     }
   }
 }
