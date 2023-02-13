a7b1a58a158aa0ebc3f01536e49676e69d097281
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 18:21:13 +0200

    inject initializeDependencies

--- lib/core/utility/injector.dart
+++ lib/core/utility/injector.dart
@@ -1,8 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:youtube/data/data_sources/remote/api/youtube_apis.dart';
+import 'package:youtube/data/data_sources/remote/api/channel/channel_apis.dart';
+import 'package:youtube/data/data_sources/remote/api/videos/videos_apis.dart';
+import 'package:youtube/data/repositories_impl/channel_details_repo_impl.dart';
 import 'package:youtube/data/repositories_impl/videos_details_repo_impl.dart';
+import 'package:youtube/domain/repositories/channel_details_repository.dart';
 import 'package:youtube/domain/repositories/videos_details_repository.dart';
+import 'package:youtube/domain/use_cases/channel_details/channel_sub_details_use_case.dart';
 import 'package:youtube/domain/use_cases/videos_details/get_video_details_usecase.dart';
 import 'package:youtube/domain/use_cases/videos_details/most_popular_videos_use_case.dart';
 import 'package:youtube/presentation/cubit/videos_details/videos_details_cubit.dart';
@@ -10,11 +14,13 @@ import 'package:youtube/presentation/cubit/videos_details/videos_details_cubit.d
 final injector = GetIt.I;
 
 Future<void> initializeDependencies() async {
-
   /// =============================== Data source =========================================>
 
-  injector.registerLazySingleton<YoutubeAPIs>(
-      () => YoutubeAPIs(createAndSetupDio()));
+  injector
+      .registerLazySingleton<VideosAPIs>(() => VideosAPIs(createAndSetupDio()));
+
+  injector.registerLazySingleton<ChannelAPIs>(
+      () => ChannelAPIs(createAndSetupDio()));
 
   /// =============================== Repository =========================================>
 
@@ -23,6 +29,9 @@ Future<void> initializeDependencies() async {
   injector.registerLazySingleton<VideoDetailsRepository>(
     () => VideoDetailsRepoImpl(injector()),
   );
+  injector.registerLazySingleton<ChannelDetailsRepository>(
+    () => ChannelDetailsRepoImpl(injector()),
+  );
 
   // *
   /// ================================ Blocs ========================================>
@@ -30,8 +39,13 @@ Future<void> initializeDependencies() async {
   injector.registerLazySingleton<VideoDetailsUseCase>(
     () => VideoDetailsUseCase(injector()),
   );
+
   injector.registerLazySingleton<MostPopularVideosUseCase>(
     () => MostPopularVideosUseCase(injector()),
+  );
+
+  injector.registerLazySingleton<ChannelSubDetailsUseCase>(
+    () => ChannelSubDetailsUseCase(injector()),
   );
 
   // *
@@ -39,7 +53,7 @@ Future<void> initializeDependencies() async {
 
   // auth cubit
   injector.registerFactory<VideosDetailsCubit>(
-    () => VideosDetailsCubit(injector(),injector()),
+    () => VideosDetailsCubit(injector(), injector(), injector()),
   );
 
   // *
