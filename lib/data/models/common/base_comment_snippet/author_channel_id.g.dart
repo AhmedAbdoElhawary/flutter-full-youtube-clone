8f02e16ad0f03af7a55964ab539271f506b5bb3a
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 21:10:47 +0200

    create _$AuthorChannelIdFromJson

---
+++ lib/data/models/common/base_comment_snippet/author_channel_id.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'author_channel_id.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthorChannelId _$AuthorChannelIdFromJson(Map<String, dynamic> json) =>
+    AuthorChannelId(
+      value: json['value'] as String?,
+    );
