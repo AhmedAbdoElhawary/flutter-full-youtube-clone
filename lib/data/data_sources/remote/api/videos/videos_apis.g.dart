46ad7f9ab433d9bc1ea7ef1488c35881b42e0be3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-13 21:14:16 +0200

    refactoring the code

--- lib/data/data_sources/remote/api/videos/videos_apis.g.dart
+++ lib/data/data_sources/remote/api/videos/videos_apis.g.dart
@@ -43,35 +43,6 @@ class _VideosAPIs implements VideosAPIs {
     return value;
   }
 
-  @override
-  Future<VideosDetails> getVideoDetails({
-    apiKey = apiKey,
-    required videoId,
-  }) async {
-    const _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{
-      r'key': apiKey,
-      r'id': videoId,
-    };
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<VideosDetails>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
-            .compose(
-              _dio.options,
-              'videos?part=contentDetails,statistics,snippet',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = VideosDetails.fromJson(_result.data!);
-    return value;
-  }
-
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
