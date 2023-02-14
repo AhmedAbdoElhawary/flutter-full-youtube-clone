d350becc2ac3f5d8857a9bf2ea5d741f8a43afb4
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 00:48:56 +0200

    change video_details to videos_details

--- lib/domain/repositories/single_video_details_repository.dart
+++ lib/domain/repositories/single_video_details_repository.dart
@@ -1,7 +1,7 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/data/models/comment_details/comment_details.dart';
 import 'package:youtube/data/models/reply_details/reply_details.dart';
-import 'package:youtube/data/models/video_details/video_details.dart';
+import 'package:youtube/data/models/videos_details/videos_details.dart';
 
 abstract class SingleVideoDetailsRepository {
   Future<ApiResult<VideosDetails>> getVideoDetails({required String videoId});
