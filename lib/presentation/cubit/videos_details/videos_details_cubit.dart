25423524359ec0b4fabb8b507049645fbe043fd8
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 00:48:42 +0200

    change video_details to videos_details

--- lib/presentation/cubit/videos_details/videos_details_cubit.dart
+++ lib/presentation/cubit/videos_details/videos_details_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/functions/network_exceptions.dart';
-import 'package:youtube/data/models/video_details/video_details.dart';
+import 'package:youtube/data/models/videos_details/videos_details.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube/domain/use_cases/videos_details/most_popular_videos_use_case.dart';
 part 'videos_details_state.dart';
@@ -11,8 +11,7 @@ part 'videos_details_cubit.freezed.dart';
 class VideosDetailsCubit extends Cubit<VideosDetailsState> {
   final MostPopularVideosUseCase _mostPopularVideosUseCase;
 
-  VideosDetailsCubit(
-      this._mostPopularVideosUseCase)
+  VideosDetailsCubit(this._mostPopularVideosUseCase)
       : super(const VideosDetailsState.initial());
 
   static VideosDetailsCubit get(BuildContext context) =>
