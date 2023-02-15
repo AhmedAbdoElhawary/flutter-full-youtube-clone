dfe87d67fedd75be6f9cb209de10154d8dd2437e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:34:23 +0200

    create ChannelPlayListDetailsRepository

---
+++ lib/domain/repositories/channel/channel_playlist_repository.dart
@@ -0,0 +1,11 @@
+import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlist_videos.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlists.dart';
+
+abstract class ChannelPlayListDetailsRepository {
+  Future<ApiResult<PlayLists>> getChannelPlayLists({required String channelId});
+
+  Future<ApiResult<PlayListVideos>> getChannelPlayListItem({
+    required String playlistId,
+  });
+}
