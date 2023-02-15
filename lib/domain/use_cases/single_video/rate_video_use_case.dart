15e2897e132e43b57a665a8eadb8ff0c86e0eb22
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:34:54 +0200

    create RateVideoUseCase

---
+++ lib/domain/use_cases/single_video/rate_video_use_case.dart
@@ -0,0 +1,29 @@
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/domain/repositories/video_comment_details/single_video_details_repository.dart';
+
+class RateVideoUseCase implements UseCase<void, RateVideoUseCaseParameter> {
+  final SingleVideoDetailsRepository _videoDetailsRepository;
+
+  RateVideoUseCase(this._videoDetailsRepository);
+
+  @override
+  Future<void> call({required RateVideoUseCaseParameter params}) {
+    return _videoDetailsRepository.rateVideo(
+        videoId: params.videoId, rating: params.rating);
+  }
+}
+
+/// [rating]	string
+/// Specifies the rating to record.
+///
+/// Acceptable values are:
+/// dislike – Records that the authenticated user disliked the video.
+/// like – Records that the authenticated user liked the video.
+/// none – Removes any rating that the authenticated user had previously set for the video.
+
+class RateVideoUseCaseParameter {
+  String videoId;
+  String rating;
+
+  RateVideoUseCaseParameter({required this.videoId, required this.rating});
+}
