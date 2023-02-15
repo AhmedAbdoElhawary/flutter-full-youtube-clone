8aff428e761b40c7afe9ffd0f1bb673aa55b09fc
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:36:23 +0200

    add subscribeToChannel,deleteSubscription in ChannelAPIs

--- lib/data/data_sources/remote/api/channel/channel_apis.g.dart
+++ lib/data/data_sources/remote/api/channel/channel_apis.g.dart
@@ -49,6 +49,60 @@ class _ChannelAPIs implements ChannelAPIs {
     return value;
   }
 
+  @override
+  Future<void> subscribeToChannel({
+    apiKey = apiKey,
+    required body,
+    required accessToken,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'key': apiKey};
+    final _headers = <String, dynamic>{r'Authorization': accessToken};
+    _headers.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    _data.addAll(body);
+    await _dio.fetch<void>(_setStreamType<void>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'subscriptions?part=snippet',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    return null;
+  }
+
+  @override
+  Future<void> deleteSubscription({
+    required id,
+    required accessToken,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'id': id,
+      r'access_token': accessToken,
+    };
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    await _dio.fetch<void>(_setStreamType<void>(Options(
+      method: 'DELETE',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'subscriptions',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    return null;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
