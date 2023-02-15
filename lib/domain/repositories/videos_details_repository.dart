5806a2348250462eb519c127f07d5467619a8e25
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:39:46 +0200

    add getAllVideos,getAllShortVideos,getCompleteVideosDetailsOfThoseIds in VideosDetailsRepository

--- lib/domain/repositories/videos_details_repository.dart
+++ lib/domain/repositories/videos_details_repository.dart
@@ -1,6 +1,10 @@
 import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/data/models/common/videos_ids/videos_ids.dart';
 import 'package:youtube/data/models/videos_details/videos_details.dart';
 
 abstract class VideosDetailsRepository {
+  Future<ApiResult<VideosDetails>> getAllVideos();
+  Future<ApiResult<VideosDetails>> getAllShortVideos();
   Future<ApiResult<VideosDetails>> getMostPopularVideos();
+  Future<VideosDetails> getCompleteVideosDetailsOfThoseIds(VideosIdsDetails videosIdsDetails);
 }
