6145827d0caf4741e9df45667c141fbfa4da4fa3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:40:27 +0200

    refactoring the code

--- lib/data/data_sources/remote/api/channel/channel_videos/channel_videos_apis.dart
+++ lib/data/data_sources/remote/api/channel/channel_videos/channel_videos_apis.dart
@@ -2,31 +2,23 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:youtube/core/utility/constants.dart';
 import 'package:youtube/core/utility/private_key.dart';
-import 'package:youtube/data/models/channel_details/channel_videos_ids/channel_videos_ids.dart';
-import 'package:youtube/data/models/videos_details/videos_details.dart';
+import 'package:youtube/data/models/common/videos_ids/videos_ids.dart';
 part 'channel_videos_apis.g.dart';
 
 @RestApi(baseUrl: youtubeBaseUrl)
 abstract class ChannelVideosAPIs {
   factory ChannelVideosAPIs(Dio dio, {String baseUrl}) = _ChannelVideosAPIs;
-
-  @GET("search?&order=date&type=video&part=id&maxResults=50")
-  Stream<ChannelVideosIds> getChannelVideosIds({
+  @GET("search?type=video&part=id&maxResults=50")
+  Future<VideosIdsDetails> getAllChannelVideosIds({
     @Query("key") final String apiKey = apiKey,
     @Query("channelId") required String channelId,
+    @Query("order") String orderVideos = "date",
   });
 
-  @GET("videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50")
-  Stream<VideosDetails> getChannelVideos({
+  @GET("search?type=video&part=id&maxResults=50&videoDuration=short")
+  Future<VideosIdsDetails> getAllChannelShortVideosIds({
     @Query("key") final String apiKey = apiKey,
-    @Query("id") required String videosIds,
-    @Query("order") String orderVideos ="date",
-  });
-
-  @GET("search?type=video&videoDuration=short&part=snippet&maxResults=50")
-  Stream<VideosDetails> getChannelShortVideos({
-    @Query("key") final String apiKey = apiKey,
-    @Query("id") required String videosIds,
-    @Query("order") String orderVideos ="date",
+    @Query("channelId") required String channelId,
+    @Query("order") String orderVideos = "date",
   });
 }
