8a749c0ecde1120b6b09146fabec7511795fbd9e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:39:04 +0200

    add allVideosLoaded,allShortVideosLoaded in VideosDetailsState

--- lib/presentation/cubit/videos_details/videos_details_state.dart
+++ lib/presentation/cubit/videos_details/videos_details_state.dart
@@ -6,6 +6,12 @@ class VideosDetailsState with _$VideosDetailsState {
 
   const factory VideosDetailsState.loading() = Loading;
 
+  const factory VideosDetailsState.allVideosLoaded(
+      VideosDetails mostPopularVideos) = AllVideosLoaded;
+
+  const factory VideosDetailsState.allShortVideosLoaded(
+      VideosDetails mostPopularVideos) = AllShortVideosLoaded;
+
   const factory VideosDetailsState.mostPopularVideosLoaded(
       VideosDetails mostPopularVideos) = MostPopularVideosLoaded;
 
