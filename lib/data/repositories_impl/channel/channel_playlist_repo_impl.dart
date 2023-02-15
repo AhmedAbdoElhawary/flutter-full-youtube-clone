f2f082528755aaa1d345cafc1ac52ae5ab682302
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:34:13 +0200

    create ChannelPlayListDetailsRepoImpl

---
+++ lib/data/repositories_impl/channel/channel_playlist_repo_impl.dart
@@ -0,0 +1,39 @@
+import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/core/functions/network_exceptions.dart';
+import 'package:youtube/data/data_sources/remote/api/channel/channel_playlist/channel_playlist_apis.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlist_videos.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlists.dart';
+import 'package:youtube/domain/repositories/channel/channel_playlist_repository.dart';
+
+class ChannelPlayListDetailsRepoImpl
+    implements ChannelPlayListDetailsRepository {
+  final ChannelPlayListAPIs _channelPlayListAPIs;
+
+  ChannelPlayListDetailsRepoImpl(this._channelPlayListAPIs);
+
+  @override
+  Future<ApiResult<PlayListVideos>> getChannelPlayListItem(
+      {required String playlistId}) async {
+    try {
+      PlayListVideos channelPlayListItem = await _channelPlayListAPIs
+          .getChannelPlayListItem(playlistId: playlistId);
+
+      return ApiResult.success(channelPlayListItem);
+    } catch (e) {
+      return ApiResult.failure(NetworkExceptions.getDioException(e));
+    }
+  }
+
+  @override
+  Future<ApiResult<PlayLists>> getChannelPlayLists(
+      {required String channelId}) async {
+    try {
+      PlayLists playLists =
+          await _channelPlayListAPIs.getChannelPlayLists(channelId: channelId);
+
+      return ApiResult.success(playLists);
+    } catch (e) {
+      return ApiResult.failure(NetworkExceptions.getDioException(e));
+    }
+  }
+}
