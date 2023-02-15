df0ae470c5e0ca9db20070f9c1a68c6d459b3b9f
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:34:44 +0200

    create CommentAPIs

---
+++ lib/data/data_sources/remote/api/single_video/commet/comment_apis.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:youtube/core/utility/constants.dart';
+import 'package:youtube/core/utility/private_key.dart';
+import 'package:youtube/data/models/comment_details/comment_body/comment_body.dart';
+import 'package:youtube/data/models/comment_details/comment_details.dart';
+import 'package:youtube/data/models/reply_details/reply_details.dart';
+part 'comment_apis.g.dart';
+
+@RestApi(baseUrl: youtubeBaseUrl)
+abstract class CommentAPIs {
+  factory CommentAPIs(Dio dio, {String baseUrl}) = _CommentAPIs;
+
+  @GET("commentThreads?part=snippet&maxResults=100")
+  Future<CommentDetails> getAllComments({
+    @Query("key") final String apiKey = apiKey,
+    @Query("videoId") required String videoId,
+  });
+
+  @GET("commentThreads?part=snippet&maxResults=1")
+  Future<CommentDetails> getFirstComment({
+    @Query("key") final String apiKey = apiKey,
+    @Query("videoId") required String videoId,
+  });
+
+  @GET("commentThreads?part=replies&maxResults=100")
+  Future<ReplyDetails> getRepliesForThisComment({
+    @Query("key") final String apiKey = apiKey,
+    @Query("id") required String commentId,
+  });
+
+  /// [insertComment] for reply and comment: just set parentId if you want a reply
+  @POST("comments?part=snippet")
+  Future<void> insertComment(
+      {@Query("access_token") required String accessToken,
+      @Query("key") final String apiKey = apiKey,
+      @Body() required CommentBody commentBody});
+}
