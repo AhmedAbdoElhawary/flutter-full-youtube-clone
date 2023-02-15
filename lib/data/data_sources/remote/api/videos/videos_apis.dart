db6e76e6819a78c4b2d07f47d979592145726dd2
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:38:02 +0200

    add getAllShortVideosIds in VideosAPIs

--- lib/data/data_sources/remote/api/videos/videos_apis.dart
+++ lib/data/data_sources/remote/api/videos/videos_apis.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:youtube/core/utility/constants.dart';
 import 'package:youtube/core/utility/private_key.dart';
+import 'package:youtube/data/models/common/videos_ids/videos_ids.dart';
 import 'package:youtube/data/models/videos_details/videos_details.dart';
 part 'videos_apis.g.dart';
 
@@ -9,15 +10,26 @@ part 'videos_apis.g.dart';
 abstract class VideosAPIs {
   factory VideosAPIs(Dio dio, {String baseUrl}) = _VideosAPIs;
 
-  @GET(
-      "search?part=snippet&location=30.044420,31.235712&locationRadius=1000km&type=video&maxResults=50")
-  Stream<VideosDetails> getAllVideos({
+  @GET("search?part=id&maxResults=50&regionCode=EG")
+  Future<VideosIdsDetails> getAllVideosIds({
     @Query("key") final String apiKey = apiKey,
+  });
+
+  @GET("search?part=id&maxResults=50&regionCode=EG&videoDuration=short")
+  Future<VideosIdsDetails> getAllShortVideosIds({
+    @Query("key") final String apiKey = apiKey,
+  });
+
+  /// [videosIds] add ids like this: id1,id2,id3,id4
+  @GET("search?part=id&maxResults=50&regionCode=EG")
+  Future<VideosDetails> getVideosOfThoseIds({
+    @Query("key") final String apiKey = apiKey,
+    @Query("key") required String videosIds,
   });
 
   @GET(
       "videos?part=contentDetails,statistics,snippet&chart=mostPopular&maxResults=50&regionCode=EG")
-  Stream<VideosDetails> getMostPopularVideos({
+  Future<VideosDetails> getMostPopularVideos({
     @Query("key") final String apiKey = apiKey,
   });
 }
