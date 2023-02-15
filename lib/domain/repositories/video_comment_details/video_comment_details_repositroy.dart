977ce6747551edcb687514691cff7d69ebe4c41b
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-15 22:39:12 +0200

    add allVideosLoaded,allShortVideosLoaded in VideosDetailsState

--- lib/domain/repositories/video_comment_details/video_comment_details_repositroy.dart
+++ lib/domain/repositories/video_comment_details/video_comment_details_repositroy.dart
@@ -1,10 +1,8 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/data/models/comment_details/comment_details.dart';
 import 'package:youtube/data/models/reply_details/reply_details.dart';
-import 'package:youtube/data/models/videos_details/videos_details.dart';
 
-abstract class SingleVideoDetailsRepository {
-  Future<ApiResult<VideosDetails>> getVideoDetails({required String videoId});
+abstract class VideoCommentDetailsRepository {
   Future<ApiResult<CommentDetails>> getAllComments({required String videoId});
   Future<ApiResult<CommentDetails>> getFirstComment({required String videoId});
   Future<ApiResult<ReplyDetails>> getRepliesForThisComment(
