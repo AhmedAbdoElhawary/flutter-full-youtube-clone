87b877accda1886e9965420c6e0fb49a0997a59d
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-16 23:43:28 +0200

    create ChannelDetailsState

---
+++ lib/presentation/cubit/channel/channel_details_state.dart
@@ -0,0 +1,22 @@
+part of 'channel_details_cubit.dart';
+
+@freezed
+class ChannelDetailsState with _$ChannelDetailsState {
+  const factory ChannelDetailsState.initial() = _Initial;
+
+  const factory ChannelDetailsState.loading() = Loading;
+
+  const factory ChannelDetailsState.channelSubDetailsLoaded(
+      ChannelSubDetails channelSubDetails) = ChannelSubDetailsLoaded;
+
+  const factory ChannelDetailsState.subscribeToChannelLoaded() =
+      SubscribeToChannelLoaded;
+
+  const factory ChannelDetailsState.deleteSubscriptionLoaded() =
+      DeleteSubscriptionLoaded;
+
+  const factory ChannelDetailsState.mySubscriptionsChannelsLoaded() =
+      MySubscriptionsChannelsLoaded;
+
+  const factory ChannelDetailsState.error(String error) = Error;
+}
