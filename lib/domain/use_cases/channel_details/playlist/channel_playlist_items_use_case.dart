f3f0425141713d14e3e3e770e244f9f8ba49a55e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-16 23:40:30 +0200

    create ChannelPlayListItemsUseCase

---
+++ lib/domain/use_cases/channel_details/playlist/channel_playlist_items_use_case.dart
@@ -0,0 +1,26 @@
+import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlist_videos.dart';
+import 'package:youtube/domain/repositories/channel/channel_playlist_repository.dart';
+
+class ChannelPlayListItemsUseCase
+    implements
+        UseCase<ApiResult<PlayListVideos>,
+            ChannelPlayListItemUseCaseParameter> {
+  final ChannelPlayListDetailsRepository _listDetailsRepository;
+
+  ChannelPlayListItemsUseCase(this._listDetailsRepository);
+
+  @override
+  Future<ApiResult<PlayListVideos>> call(
+      {required ChannelPlayListItemUseCaseParameter params}) {
+    return _listDetailsRepository.getChannelPlayListItem(
+        playlistId: params.playlistId);
+  }
+}
+
+class ChannelPlayListItemUseCaseParameter {
+  String playlistId;
+
+  ChannelPlayListItemUseCaseParameter({required this.playlistId});
+}
