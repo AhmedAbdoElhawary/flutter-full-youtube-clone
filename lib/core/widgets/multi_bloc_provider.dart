06684e6ff7c98d97e4af1a4b643afb20da79f851
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-16 23:45:59 +0200

    feed MultiBlocProvider

--- lib/core/widgets/multi_bloc_provider.dart
+++ lib/core/widgets/multi_bloc_provider.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube/core/utility/injector.dart';
-import 'package:youtube/presentation/cubit/channel_details/channel_details_cubit.dart';
-import 'package:youtube/presentation/cubit/single_video_details/single_video_cubit.dart';
-import 'package:youtube/presentation/cubit/videos_details/videos_details_cubit.dart';
+import 'package:youtube/presentation/cubit/channel/channel_details_cubit.dart';
+import 'package:youtube/presentation/cubit/channel/channel_videos/channel_videos_cubit.dart';
+import 'package:youtube/presentation/cubit/channel/playlist/play_list_cubit.dart';
+import 'package:youtube/presentation/cubit/search/search_cubit.dart';
+import 'package:youtube/presentation/cubit/single_video/single_video_cubit.dart';
+import 'package:youtube/presentation/cubit/videos/popular_videos/popular_videos_cubit.dart';
+import 'package:youtube/presentation/cubit/videos/videos_details_cubit.dart';
 
 class MultiBlocs extends StatelessWidget {
   final Widget materialApp;
@@ -21,6 +25,18 @@ class MultiBlocs extends StatelessWidget {
       ),
       BlocProvider<SingleVideoCubit>(
         create: (context) => injector<SingleVideoCubit>(),
+      ),
+      BlocProvider<ChannelVideosCubit>(
+        create: (context) => injector<ChannelVideosCubit>(),
+      ),
+      BlocProvider<PlayListCubit>(
+        create: (context) => injector<PlayListCubit>(),
+      ),
+      BlocProvider<SearchCubit>(
+        create: (context) => injector<SearchCubit>(),
+      ),
+      BlocProvider<PopularVideosCubit>(
+        create: (context) => injector<PopularVideosCubit>(),
       ),
     ], child: materialApp);
   }
