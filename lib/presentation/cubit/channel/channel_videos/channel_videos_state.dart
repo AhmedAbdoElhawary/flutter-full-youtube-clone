6fa2a00c4cdd878710b1d582aa9dbe9faeac3703
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-16 23:40:58 +0200

    create ChannelVideosState

---
+++ lib/presentation/cubit/channel/channel_videos/channel_videos_state.dart
@@ -0,0 +1,23 @@
+part of 'channel_videos_cubit.dart';
+
+@freezed
+class ChannelVideosState with _$ChannelVideosState {
+  const factory ChannelVideosState.initial() = _Initial;
+
+  const factory ChannelVideosState.loading() = Loading;
+
+  const factory ChannelVideosState.popularVideosLoaded(
+      VideosDetails videoDetails) = PopularVideosLoaded;
+
+  const factory ChannelVideosState.shortPopularVideosLoaded(
+      VideosDetails videoDetails) = ShortPopularVideosLoaded;
+
+  const factory ChannelVideosState.shortVideosLoaded(
+      VideosDetails videoDetails) = ShortVideosLoaded;
+
+  const factory ChannelVideosState.channelVideosLoaded(
+      VideosDetails videoDetails) = ChannelVideosLoaded;
+
+  const factory ChannelVideosState.error(String error) =
+      Error;
+}
