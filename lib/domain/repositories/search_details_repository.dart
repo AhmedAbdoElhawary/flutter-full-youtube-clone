17f69e3ab2976056a2c2deee44e53e3fc9c9a831
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-18 18:59:04 +0200

    change failure as String to NetworkExceptionModel

--- lib/domain/repositories/search_details_repository.dart
+++ lib/domain/repositories/search_details_repository.dart
@@ -1,7 +1,9 @@
 import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/data/models/suggestion_texts/suggestion_texts.dart';
 import 'package:youtube/data/models/videos_details/videos_details.dart';
 
 abstract class SearchDetailsRepository {
+  Future<ApiResult<SuggestionTexts>> getSuggestionTexts({required String text});
   Future<ApiResult<VideosDetails>> searchForThisSentence(
       {required String sentence});
   Future<ApiResult<VideosDetails>> getRelatedVideosToThisVideo(
