5388ef1cfc3ef383e52095ef7f973c27b14f17a1
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-18 18:56:51 +0200

    create SuggestionSearchTextsUseCase

---
+++ lib/domain/use_cases/search_details/suggestion_search_texts_use_case.dart
@@ -0,0 +1,24 @@
+import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/data/models/suggestion_texts/suggestion_texts.dart';
+import 'package:youtube/domain/repositories/search_details_repository.dart';
+
+class SuggestionSearchTextsUseCase
+    implements
+        UseCase<ApiResult<SuggestionTexts>,
+            SuggestionSearchTextsUseCaseParameter> {
+  final SearchDetailsRepository _searchDetailsRepository;
+
+  SuggestionSearchTextsUseCase(this._searchDetailsRepository);
+
+  @override
+  Future<ApiResult<SuggestionTexts>> call(
+      {required SuggestionSearchTextsUseCaseParameter params}) {
+    return _searchDetailsRepository.getSuggestionTexts(text: params.text);
+  }
+}
+
+class SuggestionSearchTextsUseCaseParameter {
+  String text;
+  SuggestionSearchTextsUseCaseParameter({required this.text});
+}
