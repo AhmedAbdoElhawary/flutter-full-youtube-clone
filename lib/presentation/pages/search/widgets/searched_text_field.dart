11fd74b64ddd16c3afccdf900a54c2d84bd08abc
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-18 18:58:57 +0200

    change failure as String to NetworkExceptionModel

--- lib/presentation/pages/search/widgets/searched_text_field.dart
+++ lib/presentation/pages/search/widgets/searched_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube/core/resources/color_manager.dart';
 import 'package:youtube/core/resources/styles_manager.dart';
+import 'package:youtube/presentation/cubit/search/search_cubit.dart';
 
 import '../../../../config/routes/route_app.dart';
 import '../search_page.dart';
@@ -15,12 +17,15 @@ class SearchTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      onChanged: (input) {},
+      onChanged: (input) {
+        BlocProvider.of<SearchCubit>(context).getSuggestionTexts(input);
+      },
       readOnly: enableOnTap,
       onTap: !enableOnTap
           ? null
           : () {
-              Go(context).to(materialRoute: true,SearchPage(text: controller.text));
+              Go(context)
+                  .to(materialRoute: true, SearchPage(text: controller.text));
             },
       controller: controller,
       cursorColor: ColorManager(context).black,
