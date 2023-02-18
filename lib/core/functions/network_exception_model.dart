8b42fbc38f78d65715e74fb5a1c7ea9d9dc7d2e5
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-18 18:56:43 +0200

    create NetworkExceptionModel

---
+++ lib/core/functions/network_exception_model.dart
@@ -0,0 +1,8 @@
+import 'package:youtube/core/functions/network_exceptions.dart';
+
+class NetworkExceptionModel {
+  String error;
+  NetworkExceptions networkExceptions;
+
+  NetworkExceptionModel(this.error, this.networkExceptions);
+}
