df59fe7572f773c05deccaa6edb392187e3fd705
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-18 18:59:33 +0200

    change failure as String to NetworkExceptionModel

--- lib/presentation/cubit/channel/playlist/play_list_cubit.dart
+++ lib/presentation/cubit/channel/playlist/play_list_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/core/functions/network_exception_model.dart';
 
 import 'package:youtube/data/models/channel_details/playlists/playlist_videos.dart';
 import 'package:youtube/data/models/channel_details/playlists/playlists.dart';
