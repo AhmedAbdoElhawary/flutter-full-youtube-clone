017398d9272964b258b87f2b233b251642b6a32a
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-20 02:03:51 +0200

    create HighThumbnails

--- lib/presentation/common_widgets/circular_profile_image.dart
+++ lib/presentation/common_widgets/circular_profile_image.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube/config/routes/route_app.dart';
+import 'package:youtube/data/models/channel_details/channel_details.dart';
 
 import '../../core/resources/color_manager.dart';
 import '../pages/channel_profile/user_profile_page.dart';
@@ -9,21 +10,30 @@ class CircularProfileImage extends StatelessWidget {
   final bool enableTapping;
   final double radius;
   final String imageUrl;
+  final String channelId;
+  final ChannelDetailsItem? channelDetailsItem;
   const CircularProfileImage(
       {this.imageUrl = "",
+      this.channelDetailsItem,
       this.enableTapping = true,
       this.radius = 18,
+      this.channelId = "",
       Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: !enableTapping
-          ? null
-          : () {
-              Go(context).to(const UserProfilePage());
-            },
+      onTap: () {
+        if (!enableTapping || channelId.isEmpty) return;
+
+        Go(context).to(
+          UserProfilePage(
+            channelDetailsItem: channelDetailsItem,
+            channelId: channelId,
+          ),
+        );
+      },
       child: CircleAvatar(
         radius: radius.r,
         backgroundColor: ColorManager(context).grey,
