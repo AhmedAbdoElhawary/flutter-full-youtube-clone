537f6547a1b96b6bf3259b5d709f0b2de9e22872
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-20 02:03:55 +0200

    create HighThumbnails

---
+++ lib/data/models/common/thumbnail_details/high_thumbnail_details.dart
@@ -0,0 +1,15 @@
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:youtube/data/models/common/thumbnail_details/thumbnail_details.dart';
+
+part 'high_thumbnail_details.g.dart';
+@JsonSerializable(includeIfNull: true, createToJson: false)
+class HighThumbnails {
+  ThumbnailDetails? medium;
+  ThumbnailDetails? high;
+
+  HighThumbnails(this.medium, this.high);
+
+  factory HighThumbnails.fromJson(Map<String, dynamic> json) =>
+      _$HighThumbnailsFromJson(json);
+}
