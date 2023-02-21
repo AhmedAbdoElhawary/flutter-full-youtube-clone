825771c03e4f9c721fce82e52aa6861dadc5f16e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-21 20:42:40 +0200

    reduce maxResults from 50 to 2

--- lib/data/data_sources/remote/api/channel/channel_videos/channel_videos_apis.dart
+++ lib/data/data_sources/remote/api/channel/channel_videos/channel_videos_apis.dart
@@ -2,20 +2,20 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:youtube/core/utility/constants.dart';
 import 'package:youtube/core/utility/private_key.dart';
-import 'package:youtube/data/models/videos_details/searched_video_details/searched_video_details.dart';
+import 'package:youtube/data/models/searched_video_details/searched_video_details.dart';
 part 'channel_videos_apis.g.dart';
 
 @RestApi(baseUrl: youtubeBaseUrl)
 abstract class ChannelVideosAPIs {
   factory ChannelVideosAPIs(Dio dio, {String baseUrl}) = _ChannelVideosAPIs;
-  @GET("search?part=snippet&type=video&maxResults=50")
+  @GET("search?part=snippet&type=video&maxResults=2")
   Future<SearchedVideosDetails> getAllChannelVideosIds({
     @Query("key") final String apiKey = apiKey,
     @Query("channelId") required String channelId,
     @Query("order") String orderVideos = "date",
   });
 
-  @GET("search?part=snippet&type=video&maxResults=50&videoDuration=short")
+  @GET("search?part=snippet&type=video&maxResults=2&videoDuration=short")
   Future<SearchedVideosDetails> getAllChannelShortVideosIds({
     @Query("key") final String apiKey = apiKey,
     @Query("channelId") required String channelId,
