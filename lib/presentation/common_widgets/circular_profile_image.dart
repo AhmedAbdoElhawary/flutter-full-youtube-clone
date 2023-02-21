8e5aac2bb9d069474f9dde0298ac1d767f7eab54
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-21 20:42:50 +0200

    reduce maxResults from 50 to 2

--- lib/presentation/common_widgets/circular_profile_image.dart
+++ lib/presentation/common_widgets/circular_profile_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube/config/routes/route_app.dart';
@@ -6,7 +7,7 @@ import 'package:youtube/data/models/channel_details/channel_details.dart';
 import '../../core/resources/color_manager.dart';
 import '../pages/channel_profile/user_profile_page.dart';
 
-class CircularProfileImage extends StatelessWidget {
+class CircularProfileImage extends StatefulWidget {
   final bool enableTapping;
   final double radius;
   final String imageUrl;
@@ -22,22 +23,41 @@ class CircularProfileImage extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<CircularProfileImage> createState() => _CircularProfileImageState();
+}
+
+class _CircularProfileImageState extends State<CircularProfileImage> {
+  void cacheImage() {
+    if (widget.imageUrl.isNotEmpty) {
+      precacheImage(NetworkImage(widget.imageUrl), context);
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    cacheImage();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        if (!enableTapping || channelId.isEmpty) return;
+        if (!widget.enableTapping || widget.channelId.isEmpty) return;
 
         Go(context).to(
           UserProfilePage(
-            channelDetailsItem: channelDetailsItem,
-            channelId: channelId,
+            channelDetailsItem: widget.channelDetailsItem,
+            channelId: widget.channelId,
           ),
         );
       },
       child: CircleAvatar(
-        radius: radius.r,
+        radius: widget.radius.r,
         backgroundColor: ColorManager(context).grey,
-        backgroundImage: imageUrl.isNotEmpty ? NetworkImage(imageUrl) : null,
+        backgroundImage: widget.imageUrl.isNotEmpty
+            ? CachedNetworkImageProvider(widget.imageUrl)
+            : null,
       ),
     );
   }
