c420207abc55fdda32d236e4699c1c6cc71c669e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-21 20:43:46 +0200

    feed the screen with actual data

--- lib/presentation/pages/channel_profile/user_profile_page.dart
+++ lib/presentation/pages/channel_profile/user_profile_page.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:youtube/core/functions/network_exceptions.dart';
-import 'package:youtube/core/functions/reformat/views_reformat.dart';
 import 'package:youtube/core/resources/styles_manager.dart';
 import 'package:youtube/data/models/channel_details/channel_details.dart';
+import 'package:youtube/data/models/channel_details/channel_sub_details/channel_sub_details_extension.dart';
 import 'package:youtube/presentation/common_widgets/circular_profile_image.dart';
 import 'package:youtube/presentation/common_widgets/custom_circle_progress.dart';
+import 'package:youtube/presentation/common_widgets/custom_network_display.dart';
 import 'package:youtube/presentation/cubit/channel/channel_details_cubit.dart';
+import 'package:youtube/presentation/pages/channel_profile/channel_profile_logic.dart';
 
 import '../../../core/resources/assets_manager.dart';
 import '../../../core/resources/color_manager.dart';
@@ -24,6 +27,7 @@ class UserProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.put(ChannelProfileLogic(), tag: "1");
     if (channelDetailsItem == null) {
       return BlocBuilder<ChannelDetailsCubit, ChannelDetailsState>(
         bloc: ChannelDetailsCubit.get(context)..getChannelSubDetails(channelId),
@@ -46,28 +50,35 @@ class UserProfilePage extends StatelessWidget {
 }
 
 class _BuildScaffold extends StatelessWidget {
-  const _BuildScaffold(this.channelDetailsItem, {Key? key}) : super(key: key);
-  final ChannelDetailsItem? channelDetailsItem;
+  const _BuildScaffold(this.channelDetails, {Key? key}) : super(key: key);
+  final ChannelDetailsItem? channelDetails;
 
   @override
   Widget build(BuildContext context) {
-    if (channelDetailsItem == null) {
+    if (channelDetails == null) {
       return const Center(child: Text("There is something wrong"));
     } else {
       return Scaffold(
-        appBar: appBar(context),
+        appBar: appBar(context, channelDetails),
         body: ProfilePage(
           isThatMyPersonalId: true,
-          widgetsAboveBio: _ButtonsAboveBio(channelDetailsItem!),
+          widgetsAboveBio: _ButtonsAboveBio(channelDetails!),
+          channelDetails: channelDetails,
         ),
       );
     }
   }
 
-  AppBar appBar(BuildContext context) {
+  AppBar appBar(BuildContext context, ChannelDetailsItem? channelDetails) {
     return AppBar(
       leadingWidth: 60.w,
       backgroundColor: ColorManager(context).white,
+      title: Text(
+        channelDetails?.getName() ?? "",
+        style: getNormalStyle(color: ColorManager(context).black, fontSize: 20),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      ),
       actions: [
         const _FavoriteIconButton(),
         Padding(
@@ -105,38 +116,42 @@ class _FavoriteIconButtonState extends State<_FavoriteIconButton> {
 }
 
 class _ButtonsAboveBio extends StatelessWidget {
-  const _ButtonsAboveBio(this.channelDetailsItem, {Key? key}) : super(key: key);
-  final ChannelDetailsItem channelDetailsItem;
+  const _ButtonsAboveBio(this.channelItem, {Key? key}) : super(key: key);
+  final ChannelDetailsItem channelItem;
 
   @override
   Widget build(BuildContext context) {
-    ChannelSnippet? snippet = channelDetailsItem.snippet;
-    String? subscribers = channelDetailsItem.statistics?.subscriberCount;
-    String? views = channelDetailsItem.statistics?.viewCount;
-
+    String? subscribers = channelItem.getSubscribersCount();
     String subscribersCount =
-        CountsReformat.basicCountFormat(subscribers ?? "");
-    subscribersCount += " subscribers";
-    String viewsCount = CountsReformat.basicCountFormat(views ?? "");
-    viewsCount += " videos";
+        subscribers == "" ? "" : "$subscribers subscribers";
+
+    String? videos = channelItem.getVideosCount();
+    String viewsCount = videos == "" ? "" : "$videos videos";
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-            width: double.infinity, height: 90.h, color: ColorManager.green),
+        SizedBox(
+          width: double.infinity,
+          height: 90.h,
+          child:
+              CustomNetworkDisplay(imageUrl: channelItem.getChannelCoverUrl()),
+        ),
         const RSizedBox(height: 10),
-        const CircularProfileImage(enableTapping: false, radius: 30),
+        CircularProfileImage(
+            enableTapping: false,
+            radius: 30,
+            imageUrl: channelItem.getProfileImageUrl()),
         const RSizedBox(height: 15),
         Text(
-          snippet?.title ?? "",
+          channelItem.getName(),
           style: getBoldStyle(color: ColorManager(context).black, fontSize: 22),
         ),
         const RSizedBox(height: 10),
         const SubscribeButton(fontSize: 15),
         const RSizedBox(height: 5),
         Text(
-          "${snippet?.customUrl ?? ""} . ${subscribers == null ? "" : subscribersCount} . ${views == null ? "" : viewsCount}",
+          "${channelItem.getCustomUserName()} . $subscribersCount . $viewsCount",
           style:
               getNormalStyle(color: ColorManager(context).black, fontSize: 12),
         ),
@@ -144,11 +159,9 @@ class _ButtonsAboveBio extends StatelessWidget {
         Padding(
           padding: REdgeInsets.symmetric(horizontal: 15),
           child: GestureDetector(
-            onTap: () {
-              /// TODO: push to about page
-            },
+            onTap: () {},
             child: Text(
-              snippet?.description ?? "",
+              channelItem.getBio(),
               overflow: TextOverflow.ellipsis,
               maxLines: 2,
               style: getNormalStyle(
