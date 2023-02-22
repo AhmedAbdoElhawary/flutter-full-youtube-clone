bfdbc81d5082750a3f4d97acb4fa335e8415d912
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-22 20:42:45 +0200

    create _PodGesturesController

---
+++ lib/presentation/custom_packages/pod_player/src/controllers/pod_gestures_controller.dart
@@ -0,0 +1,83 @@
+part of 'pod_getx_video_controller.dart';
+
+class _PodGesturesController extends _PodVideoQualityController {
+  //double tap
+  Timer? leftDoubleTapTimer;
+  Timer? rightDoubleTapTimer;
+  int leftDoubleTapduration = 0;
+  int rightDubleTapduration = 0;
+  bool isLeftDbTapIconVisible = false;
+  bool isRightDbTapIconVisible = false;
+
+  Timer? hoverOverlayTimer;
+
+  ///*handle double tap
+
+  void onLeftDoubleTap({int? seconds}) {
+    isShowOverlay(true);
+    leftDoubleTapTimer?.cancel();
+    rightDoubleTapTimer?.cancel();
+
+    isRightDbTapIconVisible = false;
+    isLeftDbTapIconVisible = true;
+    updateLeftTapDuration(
+      leftDoubleTapduration += seconds ?? doubleTapForwardSeconds,
+    );
+    seekBackward(Duration(seconds: seconds ?? doubleTapForwardSeconds));
+    leftDoubleTapTimer = Timer(const Duration(milliseconds: 1500), () {
+      isLeftDbTapIconVisible = false;
+      updateLeftTapDuration(0);
+      leftDoubleTapTimer?.cancel();
+      isShowOverlay(false);
+    });
+  }
+
+  void onRightDoubleTap({int? seconds}) {
+    isShowOverlay(true);
+    rightDoubleTapTimer?.cancel();
+    leftDoubleTapTimer?.cancel();
+
+    isLeftDbTapIconVisible = false;
+    isRightDbTapIconVisible = true;
+    updateRightTapDuration(
+      rightDubleTapduration += seconds ?? doubleTapForwardSeconds,
+    );
+    seekForward(Duration(seconds: seconds ?? doubleTapForwardSeconds));
+    rightDoubleTapTimer = Timer(const Duration(milliseconds: 1500), () {
+      isRightDbTapIconVisible = false;
+      updateRightTapDuration(0);
+      rightDoubleTapTimer?.cancel();
+      isShowOverlay(false);
+    });
+  }
+
+  void onOverlayHover() {
+    if (kIsWeb) {
+      hoverOverlayTimer?.cancel();
+      isShowOverlay(true);
+      hoverOverlayTimer = Timer(
+        const Duration(seconds: 3),
+        () => isShowOverlay(false),
+      );
+    }
+  }
+
+  void onOverlayHoverExit() {
+    if (kIsWeb) {
+      isShowOverlay(false);
+    }
+  }
+
+  ///update doubletap durations
+  void updateLeftTapDuration(int val) {
+    leftDoubleTapduration = val;
+    update(['double-tap']);
+    update(['update-all']);
+  }
+
+  void updateRightTapDuration(int val) {
+    rightDubleTapduration = val;
+    update(['double-tap']);
+    update(['update-all']);
+  }
+}
