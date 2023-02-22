0d50ac8629965bcf5d24bdbe730d381d02947b44
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-22 20:41:52 +0200

    create MobileBottomSheet

---
+++ lib/presentation/custom_packages/pod_player/src/widgets/core/overlays/mobile_bottomsheet.dart
@@ -0,0 +1,291 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:youtube/presentation/custom_packages/pod_player/src/controllers/pod_getx_video_controller.dart';
+import 'package:youtube/presentation/custom_packages/pod_player/src/widgets/material_icon_button.dart';
+import 'package:youtube/presentation/custom_packages/pod_player/src/widgets/pod_progress_bar.dart';
+
+class MobileBottomSheet extends StatelessWidget {
+  final String tag;
+
+  const
+  MobileBottomSheet({
+    Key? key,
+    required this.tag,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<PodGetXVideoController>(
+      tag: tag,
+      builder: (podCtr) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (podCtr.vimeoOrVideoUrls.isNotEmpty)
+            _bottomSheetTiles(
+              title: podCtr.podPlayerLabels.quality,
+              icon: Icons.video_settings_rounded,
+              subText: '${podCtr.vimeoPlayingVideoQuality}p',
+              onTap: () {
+                Navigator.of(context).pop();
+                Timer(const Duration(milliseconds: 100), () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => SafeArea(
+                      child: _VideoQualitySelectorMob(
+                        tag: tag,
+                        onTap: null,
+                      ),
+                    ),
+                  );
+                });
+              },
+            ),
+          _bottomSheetTiles(
+            title: podCtr.podPlayerLabels.loopVideo,
+            icon: Icons.loop_rounded,
+            subText: podCtr.isLooping
+                ? podCtr.podPlayerLabels.optionEnabled
+                : podCtr.podPlayerLabels.optionDisabled,
+            onTap: () {
+              Navigator.of(context).pop();
+              podCtr.toggleLooping();
+            },
+          ),
+          _bottomSheetTiles(
+            title: podCtr.podPlayerLabels.playbackSpeed,
+            icon: Icons.slow_motion_video_rounded,
+            subText: podCtr.currentPaybackSpeed,
+            onTap: () {
+              Navigator.of(context).pop();
+              Timer(const Duration(milliseconds: 100), () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (context) => SafeArea(
+                    child: _VideoPlaybackSelectorMob(
+                      tag: tag,
+                      onTap: null,
+                    ),
+                  ),
+                );
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  ListTile _bottomSheetTiles({
+    required String title,
+    required IconData icon,
+    String? subText,
+    void Function()? onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon),
+      onTap: onTap,
+      title: FittedBox(
+        fit: BoxFit.scaleDown,
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: [
+            Text(
+              title,
+            ),
+            if (subText != null) const SizedBox(width: 6),
+            if (subText != null)
+              const SizedBox(
+                height: 4,
+                width: 4,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            if (subText != null) const SizedBox(width: 6),
+            if (subText != null)
+              Text(
+                subText,
+                style: const TextStyle(color: Colors.grey),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _VideoQualitySelectorMob extends StatelessWidget {
+  final void Function()? onTap;
+  final String tag;
+
+  const _VideoQualitySelectorMob({
+    Key? key,
+    required this.onTap,
+    required this.tag,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final podCtr = Get.find<PodGetXVideoController>(tag: tag);
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: podCtr.vimeoOrVideoUrls
+            .map(
+              (e) => ListTile(
+                title: Text('${e.quality}p'),
+                onTap: () {
+                  onTap != null ? onTap!() : Navigator.of(context).pop();
+
+                  podCtr.changeVideoQuality(e.quality);
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _VideoPlaybackSelectorMob extends StatelessWidget {
+  final void Function()? onTap;
+  final String tag;
+
+  const _VideoPlaybackSelectorMob({
+    Key? key,
+    required this.onTap,
+    required this.tag,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final podCtr = Get.find<PodGetXVideoController>(tag: tag);
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: podCtr.videoPlaybackSpeeds
+            .map(
+              (e) => ListTile(
+                title: Text(e),
+                onTap: () {
+                  onTap != null ? onTap!() : Navigator.of(context).pop();
+                  podCtr.setVideoPlayBack(e);
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class MobileOverlayBottomControlles extends StatelessWidget {
+  final String tag;
+
+  const MobileOverlayBottomControlles({
+    Key? key,
+    required this.tag,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle durationTextStyle =
+        TextStyle(color: Colors.white70, fontSize: 12.sp);
+    const itemColor = Colors.white;
+
+    return GetBuilder<PodGetXVideoController>(
+      tag: tag,
+      id: 'full-screen',
+      builder: (podCtr) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              const RSizedBox(width: 15),
+              GetBuilder<PodGetXVideoController>(
+                tag: tag,
+                id: 'video-progress',
+                builder: (podCtr) {
+                  return Row(
+                    children: [
+                      Text(
+                        podCtr.calculateVideoDuration(podCtr.videoPosition),
+                        style: TextStyle(color: itemColor, fontSize: 12.sp),
+                      ),
+                      Text(
+                        ' / ',
+                        style: durationTextStyle,
+                      ),
+                      Text(
+                        podCtr.calculateVideoDuration(podCtr.videoDuration),
+                        style: durationTextStyle,
+                      ),
+                    ],
+                  );
+                },
+              ),
+              const Spacer(),
+              MaterialIconButton(
+                toolTipMesg: podCtr.isFullScreen
+                    ? podCtr.podPlayerLabels.exitFullScreen ??
+                        'Exit full screen${kIsWeb ? ' (f)' : ''}'
+                    : podCtr.podPlayerLabels.fullscreen ??
+                        'Fullscreen${kIsWeb ? ' (f)' : ''}',
+                color: itemColor,
+                onPressed: () {
+                  if (podCtr.isOverlayVisible) {
+                    if (podCtr.isFullScreen) {
+                      podCtr.disableFullScreen(context, tag);
+                    } else {
+                      podCtr.enableFullScreen(tag);
+                    }
+                  } else {
+                    podCtr.toggleVideoOverlay();
+                  }
+                },
+                child: Icon(
+                  podCtr.isFullScreen
+                      ? Icons.fullscreen_exit
+                      : Icons.fullscreen,
+                ),
+              ),
+              const RSizedBox(width: 6),
+            ],
+          ),
+          GetBuilder<PodGetXVideoController>(
+            tag: tag,
+            id: 'overlay',
+            builder: (podCtr) {
+              if (podCtr.isFullScreen) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 0, 12, 20),
+                  child: Visibility(
+                    visible: podCtr.isOverlayVisible,
+                    child: PodProgressBar(
+                      tag: tag,
+                      alignment: Alignment.topCenter,
+                      podProgressBarConfig: podCtr.podProgressBarConfig,
+                    ),
+                  ),
+                );
+              }
+              return PodProgressBar(
+                tag: tag,
+                alignment: Alignment.bottomCenter,
+                podProgressBarConfig: podCtr.podProgressBarConfig,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
