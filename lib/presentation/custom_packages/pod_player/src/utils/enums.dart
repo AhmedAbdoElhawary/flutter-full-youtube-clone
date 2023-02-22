e8eaa65607206fc3069a1328fbf322b9a151ae5a
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-22 20:41:31 +0200

    create PodVideoState

---
+++ lib/presentation/custom_packages/pod_player/src/utils/enums.dart
@@ -0,0 +1,16 @@
+enum PodVideoState {
+  loading,
+  playing,
+  paused,
+  error,
+}
+
+enum PodVideoPlayerType {
+  network,
+  networkQualityUrls,
+  file,
+  asset,
+  vimeo,
+  youtube,
+  vimeoPrivateVideos,
+}
