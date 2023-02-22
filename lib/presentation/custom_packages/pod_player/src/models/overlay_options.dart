a174c2804c89fefb3e52047b381ad0e3634ec05b
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-22 20:42:04 +0200

    create OverLayOptions

---
+++ lib/presentation/custom_packages/pod_player/src/models/overlay_options.dart
@@ -0,0 +1,30 @@
+import '../../pod_player.dart';
+
+class OverLayOptions {
+  final PodVideoState podVideoState;
+  final Duration videoDuration;
+  final Duration videoPosition;
+  final bool isFullScreen;
+  final bool isLooping;
+  final bool isOverlayVisible;
+  final bool isMute;
+  final bool autoPlay;
+  final String currentVideoPlaybackSpeed;
+  final List<String> videoPlayBackSpeeds;
+  final PodVideoPlayerType videoPlayerType;
+  final PodProgressBar podProgresssBar;
+  OverLayOptions({
+    required this.podVideoState,
+    required this.videoDuration,
+    required this.videoPosition,
+    required this.isFullScreen,
+    required this.isLooping,
+    required this.isOverlayVisible,
+    required this.isMute,
+    required this.autoPlay,
+    required this.currentVideoPlaybackSpeed,
+    required this.videoPlayBackSpeeds,
+    required this.videoPlayerType,
+    required this.podProgresssBar,
+  });
+}
