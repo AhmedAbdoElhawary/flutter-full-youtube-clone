d996aca9a193817b27255c7195b68c675adc1aeb
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-22 20:42:16 +0200

    create PlayVideoFrom

---
+++ lib/presentation/custom_packages/pod_player/src/models/play_video_from.dart
@@ -0,0 +1,142 @@
+import '../../pod_player.dart';
+
+class PlayVideoFrom {
+  final String? dataSource;
+  final PodVideoPlayerType playerType;
+  final VideoFormat? formatHint;
+  final String? package;
+  final dynamic file;
+  final List<VideoQalityUrls>? videoQualityUrls;
+  final Future<ClosedCaptionFile>? closedCaptionFile;
+  final VideoPlayerOptions? videoPlayerOptions;
+  final Map<String, String> httpHeaders;
+  final bool live;
+
+  const PlayVideoFrom._({
+    this.live = false,
+    this.dataSource,
+    required this.playerType,
+    this.formatHint,
+    this.package,
+    this.file,
+    this.videoQualityUrls,
+    this.closedCaptionFile,
+    this.videoPlayerOptions,
+    this.httpHeaders = const {},
+  });
+
+  factory PlayVideoFrom.network(
+    String dataSource, {
+    VideoFormat? formatHint,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+    Map<String, String> httpHeaders = const {},
+  }) {
+    return PlayVideoFrom._(
+      playerType: PodVideoPlayerType.network,
+      dataSource: dataSource,
+      formatHint: formatHint,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+      httpHeaders: httpHeaders,
+    );
+  }
+
+  factory PlayVideoFrom.asset(
+    String dataSource, {
+    String? package,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+  }) {
+    return PlayVideoFrom._(
+      playerType: PodVideoPlayerType.asset,
+      dataSource: dataSource,
+      package: package,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+    );
+  }
+
+  ///File Doesnot support web apps
+  ///[file] is `File` Datatype import it from `dart:io`
+  factory PlayVideoFrom.file(
+    dynamic file, {
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+  }) {
+    return PlayVideoFrom._(
+      file: file,
+      playerType: PodVideoPlayerType.file,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+    );
+  }
+
+  factory PlayVideoFrom.vimeo(
+    String dataSource, {
+    VideoFormat? formatHint,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+    Map<String, String> httpHeaders = const {},
+  }) {
+    return PlayVideoFrom._(
+      playerType: PodVideoPlayerType.vimeo,
+      dataSource: dataSource,
+      formatHint: formatHint,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+      httpHeaders: httpHeaders,
+    );
+  }
+
+  factory PlayVideoFrom.vimeoPrivateVideos(
+    String dataSource, {
+    VideoFormat? formatHint,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+    Map<String, String> httpHeaders = const {},
+  }) {
+    return PlayVideoFrom._(
+      playerType: PodVideoPlayerType.vimeoPrivateVideos,
+      dataSource: dataSource,
+      formatHint: formatHint,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+      httpHeaders: httpHeaders,
+    );
+  }
+  factory PlayVideoFrom.youtube(
+    String dataSource, {
+    bool live = false,
+    VideoFormat? formatHint,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+    Map<String, String> httpHeaders = const {},
+  }) {
+    return PlayVideoFrom._(
+      live: live,
+      playerType: PodVideoPlayerType.youtube,
+      dataSource: dataSource,
+      formatHint: formatHint,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+      httpHeaders: httpHeaders,
+    );
+  }
+  factory PlayVideoFrom.networkQualityUrls({
+    required List<VideoQalityUrls> videoUrls,
+    VideoFormat? formatHint,
+    Future<ClosedCaptionFile>? closedCaptionFile,
+    VideoPlayerOptions? videoPlayerOptions,
+    Map<String, String> httpHeaders = const {},
+  }) {
+    return PlayVideoFrom._(
+      playerType: PodVideoPlayerType.networkQualityUrls,
+      videoQualityUrls: videoUrls,
+      formatHint: formatHint,
+      closedCaptionFile: closedCaptionFile,
+      videoPlayerOptions: videoPlayerOptions,
+      httpHeaders: httpHeaders,
+    );
+  }
+}
