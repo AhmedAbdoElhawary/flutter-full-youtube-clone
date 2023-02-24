52f02016d1acf426352842fc100d56d70be84b53
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-24 19:42:30 +0200

    create GetVideoRatingUseCase

---
+++ lib/domain/use_cases/single_video/get_video_rating_use_case.dart
@@ -0,0 +1,24 @@
+import 'package:youtube/core/functions/api_result.dart';
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/data/models/rating_details/rating_details.dart';
+import 'package:youtube/domain/repositories/video_comment_details/single_video_details_repository.dart';
+
+class GetVideoRatingUseCase
+    implements
+        UseCase<ApiResult<RatingDetails>, GetVideoRatingUseCaseParameter> {
+  final SingleVideoDetailsRepository _videoDetailsRepository;
+
+  GetVideoRatingUseCase(this._videoDetailsRepository);
+
+  @override
+  Future<ApiResult<RatingDetails>> call(
+      {required GetVideoRatingUseCaseParameter params}) {
+    return _videoDetailsRepository.getVideoRating(videoId: params.videoId);
+  }
+}
+
+class GetVideoRatingUseCaseParameter {
+  String videoId;
+
+  GetVideoRatingUseCaseParameter({required this.videoId});
+}
