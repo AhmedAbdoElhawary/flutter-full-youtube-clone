f796f4da31b14b3a576f47bd512d844600285471
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-26 23:16:32 +0200

    change Rating to String

--- lib/domain/use_cases/channel_details/my_subscriptions_channels_use_case.dart
+++ lib/domain/use_cases/channel_details/my_subscriptions_channels_use_case.dart
@@ -1,14 +1,15 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/data/models/channel_details/my_subscriptions/my_subscriptions_details.dart';
 import 'package:youtube/domain/repositories/channel/channel_details_repository.dart';
 
-class MySubscriptionsChannelsUseCase implements UseCase<ApiResult<void>, void> {
+class MySubscriptionsChannelsUseCase implements UseCase<ApiResult<MySubscriptionsDetails>, void> {
   final ChannelDetailsRepository _channelDetailsRepository;
 
   MySubscriptionsChannelsUseCase(this._channelDetailsRepository);
 
   @override
-  Future<ApiResult<void>> call({required void params}) {
+  Future<ApiResult<MySubscriptionsDetails>> call({required void params}) {
     return _channelDetailsRepository.getMySubscriptionsChannels();
   }
 }
