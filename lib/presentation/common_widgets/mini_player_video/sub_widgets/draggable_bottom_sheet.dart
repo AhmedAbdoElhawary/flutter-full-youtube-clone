351c866ed118a5618e44defdbb72626bcb276066
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-27 18:44:14 +0200

    refactoring the code

--- lib/presentation/common_widgets/mini_player_video/sub_widgets/draggable_bottom_sheet.dart
+++ lib/presentation/common_widgets/mini_player_video/sub_widgets/draggable_bottom_sheet.dart
@@ -2,19 +2,18 @@ part of '../mini_player_video.dart';
 
 Future<void> draggableBottomSheet(BuildContext context,
     {bool isThatDescription = true}) async {
-  final logic = Get.find<MiniVideoViewLogic>(tag: "1");
 
   return showSlidingBottomSheet<void>(
     context,
     builder: (BuildContext context) => SlidingSheetDialog(
       cornerRadius: 16,
-      controller: logic.descriptionSheetController,
+      shadowColor: ColorManager(context).black54,
       backdropColor: BaseColorManager.transparent,
       minHeight: 200.h,
       color: Theme.of(context).splashColor,
       dismissOnBackdropTap: false,
       isBackdropInteractable: true,
-      snapSpec: const SnapSpec(initialSnap: 0.695, snappings: [.7, 0.695, .97]),
+      snapSpec: const SnapSpec(initialSnap: 0.694, snappings: [.7, 0.694, .962]),
       builder: (context, state) => isThatDescription
           ? const _BodyOfDescriptionSheet()
           : const _BodyOfCommentsSheet(),
