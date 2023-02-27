4a69c158ec8c498a64584a19aa933323063d3160
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-27 02:37:41 +0200

    create _HeadOfBottomSheet

---
+++ lib/presentation/common_widgets/mini_player_video/sub_widgets/bottom_sheet/head_of_bottom_sheet.dart
@@ -0,0 +1,50 @@
+part of'../../mini_player_video.dart';
+
+class _HeadOfBottomSheet extends StatelessWidget {
+  const _HeadOfBottomSheet({this.headTitle = "Description", Key? key})
+      : super(key: key);
+  final String headTitle;
+  @override
+  Widget build(BuildContext context) {
+    final miniVideoViewLogic = Get.find<MiniVideoViewLogic>(tag: "1");
+
+    return Material(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsetsDirectional.only(top: 10),
+            child: Container(
+              width: 45,
+              height: 4.5,
+              decoration: BoxDecoration(
+                color: ColorManager(context).grey2,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          Padding(
+            padding: REdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  headTitle,
+                  style: getBoldStyle(
+                      color: ColorManager(context).black, fontSize: 18),
+                ),
+                InkWell(
+                    onTap: () async {
+                      await miniVideoViewLogic.descriptionSheetController
+                          .collapse();
+                    },
+                    child: SvgPicture.asset(IconsAssets.closeIcon, height: 25)),
+              ],
+            ),
+          ),
+            Divider(color: ColorManager(context).grey2, height: 1),
+        ],
+      ),
+    );
+  }
+}
