c42a92992d2c175658c777047ccc7b8f9e9cec9e
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-27 18:42:15 +0200

    create ReadMore

---
+++ lib/presentation/common_widgets/read_more_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
+import 'package:youtube/core/resources/color_manager.dart';
+import 'package:youtube/core/resources/styles_manager.dart';
+
+class ReadMore extends StatelessWidget {
+  final String text;
+  final int timeLines;
+  const ReadMore(this.text,  {this.timeLines=3,Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ReadMoreText(
+      text,
+      trimLines: timeLines,
+      colorClickableText: ColorManager(context).grey,
+      trimMode: TrimMode.Line,
+      trimCollapsedText: " Read more",
+      trimExpandedText: " Read less",
+      style: getNormalStyle(
+          color: ColorManager(context).black, fontSize: 14),
+      moreStyle:  getNormalStyle(fontSize: 14, color: ColorManager(context).grey7),
+    );
+  }
+}
