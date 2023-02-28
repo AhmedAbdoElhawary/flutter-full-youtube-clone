92f8512e777add81fc93903b2eba35cd2bbe9f6c
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-02-28 15:22:38 +0200

    pushing again some files (solve #17 branch issues)

--- lib/presentation/cubit/channel/channel_videos/channel_videos_cubit.dart
+++ lib/presentation/cubit/channel/channel_videos/channel_videos_cubit.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/functions/network_exception_model.dart';
+import 'package:youtube/data/models/channel_details/my_subscriptions/my_subscriptions_details.dart';
 
 import 'package:youtube/data/models/videos_details/videos_details.dart';
 import 'package:youtube/domain/entities/channel_details_use_case_parameters.dart';
@@ -10,6 +11,7 @@ import 'package:youtube/domain/use_cases/channel_details/channel_videos/channel_
 import 'package:youtube/domain/use_cases/channel_details/channel_videos/channel_short_poupal_videos_use_case.dart';
 import 'package:youtube/domain/use_cases/channel_details/channel_videos/channel_short_videos_use_case.dart';
 import 'package:youtube/domain/use_cases/channel_details/channel_videos/channel_videos_use_case.dart';
+import 'package:youtube/domain/use_cases/channel_details/get_videos_of_those_channels_use_case.dart';
 
 part 'channel_videos_state.dart';
 part 'channel_videos_cubit.freezed.dart';
@@ -19,9 +21,14 @@ class ChannelVideosCubit extends Cubit<ChannelVideosState> {
   final ChannelShortPopularVideosUseCase _shortPopularVideosUseCase;
   final ChannelShortVideosUseCase _shortVideosUseCase;
   final ChannelVideosUseCase _videosUseCase;
+  final GetVideosOfThoseChannelsUseCase _getVideosOfThoseChannelsUseCase;
 
-  ChannelVideosCubit(this._videosUseCase, this._shortVideosUseCase,
-      this._shortPopularVideosUseCase, this._popularVideosUseCase)
+  ChannelVideosCubit(
+      this._videosUseCase,
+      this._shortVideosUseCase,
+      this._shortPopularVideosUseCase,
+      this._popularVideosUseCase,
+      this._getVideosOfThoseChannelsUseCase)
       : super(const ChannelVideosState.initial());
 
   static ChannelVideosCubit get(BuildContext context) =>
@@ -47,8 +54,8 @@ class ChannelVideosCubit extends Cubit<ChannelVideosState> {
             params: ChannelDetailsUseCaseParameters(channelId: channelId));
 
     shortPopularVideosDetails.when(
-        success: (shortVideoDetails) =>
-            emit(ChannelVideosState.shortPopularVideosLoaded(shortVideoDetails)),
+        success: (shortVideoDetails) => emit(
+            ChannelVideosState.shortPopularVideosLoaded(shortVideoDetails)),
         failure: (exception) => emit(ChannelVideosState.error(exception)));
   }
 
@@ -73,6 +80,30 @@ class ChannelVideosCubit extends Cubit<ChannelVideosState> {
     videosDetails.when(
         success: (videoDetails) =>
             emit(ChannelVideosState.channelVideosLoaded(videoDetails)),
+        failure: (exception) => emit(ChannelVideosState.error(exception)));
+  }
+
+  Future<void> getVideosOfThoseChannels(
+      MySubscriptionsDetails? mySubscriptionsDetails) async {
+    emit(const ChannelVideosState.loading());
+    if (mySubscriptionsDetails == null) {
+      emit(const ChannelVideosState.initial());
+      return;
+    }
+    ApiResult<List<VideosDetails>> videosDetails =
+        await _getVideosOfThoseChannelsUseCase.call(
+            params: GetVideosOfThoseChannelsUseCaseParameter(
+                mySubscriptionsDetails: mySubscriptionsDetails));
+
+    videosDetails.when(
+        success: (videosDetails) {
+          List<VideoDetailsItem> videosItems = [];
+          for (final video in videosDetails) {
+            List<VideoDetailsItem>? items = video.videoDetailsItem;
+            if (items != null) videosItems.addAll(items);
+          }
+          emit(ChannelVideosState.videosOfThoseChannelsLoaded(videosItems));
+        },
         failure: (exception) => emit(ChannelVideosState.error(exception)));
   }
 }
