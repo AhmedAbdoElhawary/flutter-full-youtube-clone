c0f7f79773520495bf2b0411bc1290194d6c8d50
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 20:00:46 +0200

    reformat the code

--- lib/domain/use_cases/single_video/comment/get_all_comments_use_case.dart
+++ lib/domain/use_cases/single_video/comment/get_all_comments_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
 import 'package:youtube/data/models/comment_details/comment_details.dart';
-import 'package:youtube/domain/entities/video_details_use_case_parameter.dart';
+import 'package:youtube/domain/entities/parameters/video_details_use_case_parameter.dart';
 import 'package:youtube/domain/repositories/video_comment_details/video_comment_details_repositroy.dart';
 
 class GetAllCommentsUseCase
