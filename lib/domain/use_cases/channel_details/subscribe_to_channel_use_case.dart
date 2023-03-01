a0c15fba749fd3c361a0566815f96f3f5c7875fc
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 20:00:32 +0200

    reformat the code

--- lib/domain/use_cases/channel_details/subscribe_to_channel_use_case.dart
+++ lib/domain/use_cases/channel_details/subscribe_to_channel_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
-import 'package:youtube/domain/entities/channel_details_use_case_parameters.dart';
+import 'package:youtube/domain/entities/parameters/channel_details_use_case_parameters.dart';
 import 'package:youtube/domain/repositories/channel/channel_details_repository.dart';
 
 class SubscribeToChannelUseCase
