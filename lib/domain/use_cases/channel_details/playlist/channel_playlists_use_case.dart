fae2d790b24f7e9763d99af2ac9b58ebb3d21262
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 20:00:54 +0200

    reformat the code

--- lib/domain/use_cases/channel_details/playlist/channel_playlists_use_case.dart
+++ lib/domain/use_cases/channel_details/playlist/channel_playlists_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
 import 'package:youtube/data/models/channel_details/playlists/playlists.dart';
-import 'package:youtube/domain/entities/channel_details_use_case_parameters.dart';
+import 'package:youtube/domain/entities/parameters/channel_details_use_case_parameters.dart';
 import 'package:youtube/domain/repositories/channel/channel_playlist_repository.dart';
 
 class ChannelPlayListUseCase
