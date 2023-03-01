081e241f5324865b68df40376531d24330766150
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 20:34:44 +0200

    create ClearAllPopularChannelShortVideosUseCase

---
+++ lib/domain/use_cases/channel/channel_videos/clear/clear_all_popular_channel_short_videos_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/domain/entities/parameters/clear_channel_videos_use_case_parameters.dart';
+import 'package:youtube/domain/repositories/channel/channel_videos_repository.dart';
+
+class ClearAllPopularChannelShortVideosUseCase
+    implements UseCase<void, ClearAllChannelVideosUseCasePara> {
+  final ChannelVideosDetailsRepository _videosDetailsRepository;
+
+  ClearAllPopularChannelShortVideosUseCase(this._videosDetailsRepository);
+
+  @override
+  void call({required ClearAllChannelVideosUseCasePara params}) {
+    return _videosDetailsRepository.clearAllPopularChannelShortVideos(
+        channelId: params.channelId, clearAll: params.clearAll);
+  }
+}
