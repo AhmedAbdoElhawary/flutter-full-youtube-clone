11da66c6b6babb452acd2af95504c87c47dcaac4
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 19:12:11 +0200

    reformat the code

--- lib/presentation/common_widgets/sliver_app_bar.dart
+++ lib/presentation/common_widgets/sliver_app_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube/config/routes/route_app.dart';
+import 'package:youtube/core/resources/assets_manager.dart';
 import 'package:youtube/core/resources/color_manager.dart';
+import 'package:youtube/presentation/common_widgets/svg_icon.dart';
 
 import '../pages/search/search_page.dart';
 
@@ -13,23 +15,37 @@ class MainSliverAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       floating: true,
-      leading: const Icon(Icons.yard_outlined, size: 40),
+      leading: const SizedBox(),
+      leadingWidth: 0,
+      titleSpacing: 0,
       actionsIconTheme: const IconThemeData(size: 20),
       backgroundColor: ColorManager(context).white,
       surfaceTintColor: ColorManager(context).white,
       actions: [
-        GestureDetector(
-            onTap: () {
-              Go(context).to(const SearchPage());
-            },
-            child: const Icon(Icons.search_rounded, size: 25)),
-        const RSizedBox(width: 20),
-        const Icon(Icons.search_rounded, size: 25),
-        const RSizedBox(width: 20),
-        const Icon(Icons.search_rounded, size: 25),
-        const RSizedBox(width: 20),
-        const CircleAvatar(backgroundColor: ColorManager.teal, radius: 13),
-        const RSizedBox(width: 20),
+        Expanded(
+          child: Row(
+            children: [
+              const RSizedBox(width: 20),
+              const SvgIcon(IconsAssets.youtubeLogo, height: 20),
+
+              const Spacer(),
+              GestureDetector(
+                  onTap: () {
+                    Go(context).to(const SearchPage());
+                  },
+                  child:
+                      const SvgIcon(IconsAssets.broadcastIcon, height: 20)),
+              const RSizedBox(width: 20),
+              const SvgIcon(IconsAssets.notificationIcon),
+              const RSizedBox(width: 20),
+              const Icon(Icons.search_rounded, size: 25),
+              const RSizedBox(width: 20),
+              const CircleAvatar(
+                  backgroundColor: ColorManager.teal, radius: 13),
+              const RSizedBox(width: 20),
+            ],
+          ),
+        ),
       ],
       bottom: preferredSizeWidget,
     );
