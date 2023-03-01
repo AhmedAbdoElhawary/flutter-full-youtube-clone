0e00c4b58e94ef6bcf39d261daba7775ef38feb9
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-01 20:00:41 +0200

    reformat the code

--- lib/domain/use_cases/single_video/get_video_details_use_case.dart
+++ lib/domain/use_cases/single_video/get_video_details_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:youtube/core/functions/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
 import 'package:youtube/data/models/videos_details/videos_details.dart';
-import 'package:youtube/domain/entities/video_details_use_case_parameter.dart';
+import 'package:youtube/domain/entities/parameters/video_details_use_case_parameter.dart';
 import 'package:youtube/domain/repositories/video_comment_details/single_video_details_repository.dart';
 
 class VideoDetailsUseCase
