41f981d9c9fb17fac92ed970db6df8ac94d60316
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-02 19:19:42 +0200

    refactoring the code

--- lib/presentation/pages/channel_profile/tab_bar_views/videos_view.dart
+++ lib/presentation/pages/channel_profile/tab_bar_views/videos_view.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:youtube/core/functions/network_exceptions.dart';
 import 'package:youtube/data/models/channel_details/channel_details.dart';
 import 'package:youtube/data/models/videos_details/videos_details.dart';
 import 'package:youtube/presentation/common_widgets/custom_circle_progress.dart';
+import 'package:youtube/presentation/common_widgets/error_message_widget.dart';
 import 'package:youtube/presentation/cubit/channel/channel_videos/channel_videos_cubit.dart';
 import 'package:youtube/presentation/pages/channel_profile/channel_profile_logic.dart';
-
-import '../../../../core/resources/color_manager.dart';
-import '../../../../core/resources/styles_manager.dart';
+import 'package:youtube/presentation/pages/channel_profile/widgets/rounded_filtered_button.dart';
 import 'videos_horizontal_descriptions_list.dart';
 
 class TabBarVideosView extends StatefulWidget {
@@ -26,11 +24,17 @@ class _TabBarVideosViewState extends State<TabBarVideosView> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      return logic.getRecentlyVideosSelected
-          ? _NewestVideos(widget.channelDetails)
-          : _PopularVideos(widget.channelDetails);
-    });
+    return CustomScrollView(
+      slivers: [
+        const _FiltersButtons(),
+        Obx(() {
+          return logic.getRecentlyVideosSelected
+              ? _NewestVideos(widget.channelDetails)
+              : _PopularVideos(widget.channelDetails);
+        })
+      ],
+    );
+
   }
 
 }
@@ -49,15 +53,10 @@ class _PopularVideos extends StatelessWidget {
         if (state is PopularVideosLoaded) {
           return _VideosList(state.videoDetails);
         } else if (state is Error) {
-          return Center(
-            child: Text(
-                NetworkExceptions.getErrorMessage(
-                    state.networkExceptions.networkExceptions),
-                style: getNormalStyle(
-                    color: ColorManager(context).black, fontSize: 15)),
-          );
+          return SliverFillRemaining(
+              child: ErrorMessageWidget(state.networkExceptions));
         } else {
-          return const ThineCircularProgress();
+          return const SliverFillRemaining(child: ThineCircularProgress());
         }
       },
     );
@@ -78,15 +77,10 @@ class _NewestVideos extends StatelessWidget {
         if (state is ChannelVideosLoaded) {
           return _VideosList(state.videoDetails);
         } else if (state is Error) {
-          return Center(
-            child: Text(
-                NetworkExceptions.getErrorMessage(
-                    state.networkExceptions.networkExceptions),
-                style: getNormalStyle(
-                    color: ColorManager(context).black, fontSize: 15)),
-          );
+          return SliverFillRemaining(
+              child: ErrorMessageWidget(state.networkExceptions));
         } else {
-          return const ThineCircularProgress();
+          return const SliverFillRemaining(child: ThineCircularProgress());
         }
       },
     );
@@ -98,15 +92,16 @@ class _VideosList extends StatelessWidget {
   final VideosDetails videoDetails;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (context, index) => Padding(
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount:videoDetails.videoDetailsItem?.length ?? 0,
+            (context, index) => Padding(
               padding: REdgeInsetsDirectional.only(start: 15, top: 15),
-              child: index == 0
-                  ? const _FiltersButtons()
-                  : VideoHorizontalDescriptionsList(
-                      videoDetails.videoDetailsItem![index]),
+              child: VideoHorizontalDescriptionsList(
+                  videoDetails.videoDetailsItem![index]),
             ),
-        itemCount: videoDetails.videoDetailsItem?.length ?? 0);
+      ),
+    );
   }
 }
 
@@ -122,51 +117,25 @@ class _FiltersButtonsState extends State<_FiltersButtons> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Row(
-          children: [
-            GestureDetector(
-                onTap: () => logic.isRecentlyVideosSelected = true,
-                child: _RoundedFilteredButton(
-                    text: "Recently uploaded",
-                    isSelected: logic.getRecentlyVideosSelected)),
-            const RSizedBox(width: 10),
-            GestureDetector(
-                onTap: () => logic.isRecentlyVideosSelected = false,
-                child: _RoundedFilteredButton(
-                    text: "Popular",
-                    isSelected: !logic.getRecentlyVideosSelected)),
-          ],
-        ));
-  }
-}
-
-class _RoundedFilteredButton extends StatelessWidget {
-  const _RoundedFilteredButton({
-    Key? key,
-    required this.isSelected,
-    required this.text,
-  }) : super(key: key);
-  final bool isSelected;
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: REdgeInsets.symmetric(vertical: 8, horizontal: 13),
-      decoration: BoxDecoration(
-          color: isSelected
-              ? ColorManager(context).black
-              : ColorManager(context).grey1,
-          borderRadius: BorderRadius.circular(20.r)),
-      child: Text(
-        text,
-        style: getNormalStyle(
-            color: isSelected
-                ? ColorManager(context).white
-                : ColorManager(context).black,
-            fontSize: 13),
+    return Obx(() =>  SliverToBoxAdapter(
+      child: Padding(
+        padding: REdgeInsetsDirectional.only(start: 15, top: 15),
+        child: Row(
+              children: [
+                GestureDetector(
+                    onTap: () => logic.isRecentlyVideosSelected = true,
+                    child: RoundedFilteredButton(
+                        text: "Recently uploaded",
+                        isSelected: logic.getRecentlyVideosSelected)),
+                const RSizedBox(width: 10),
+                GestureDetector(
+                    onTap: () => logic.isRecentlyVideosSelected = false,
+                    child: RoundedFilteredButton(
+                        text: "Popular",
+                        isSelected: !logic.getRecentlyVideosSelected)),
+              ],
+            ),
       ),
-    );
+    ));
   }
 }
