ddd53eaf70e37851d67177c618c2444d7293c891
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-02 19:19:06 +0200

    create VideoDurationWidget

---
+++ lib/presentation/common_widgets/video_duration.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:youtube/core/functions/reformat/views_reformat.dart';
+import 'package:youtube/data/models/videos_details/videos_details.dart';
+
+import '../../core/resources/color_manager.dart';
+import '../../core/resources/styles_manager.dart';
+
+class VideoDurationWidget extends StatelessWidget {
+  const VideoDurationWidget(this.videoDetailsItem, {super.key});
+  final VideoDetailsItem? videoDetailsItem;
+
+  @override
+  Widget build(BuildContext context) {
+    String duration = videoDetailsItem?.contentDetails.duration ?? "";
+    String durationReformat = CountsReformat.videoDurationFormat(duration);
+    return Padding(
+      padding: REdgeInsets.all(8.0),
+      child: Container(
+        padding: REdgeInsets.symmetric(horizontal: 2, vertical: 1.5),
+        decoration: BoxDecoration(
+            color: ColorManager(context).black87,
+            borderRadius: BorderRadius.circular(2)),
+        child: Text(
+          durationReformat,
+          style:
+          getMediumStyle(color: ColorManager(context).white, fontSize: 12),
+        ),
+      ),
+    );
+  }
+}
