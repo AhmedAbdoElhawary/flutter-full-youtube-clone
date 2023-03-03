3649dbfe37bd5865eefd43a4c6b828fdb7cf0aed
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-03 23:00:27 +0200

    rebuild BaseLayout

--- lib/presentation/layouts/base_layout.dart
+++ lib/presentation/layouts/base_layout.dart
@@ -19,54 +19,74 @@ class BaseLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final miniVideoLogic = Get.find<MiniVideoViewLogic>(tag: "1");
+
+    return Material(
+      color: ColorManager(context).white,
+      shadowColor: ColorManager(context).grey1,
+      surfaceTintColor: ColorManager(context).grey1,
+      child: Stack(
+        children: [
+          const _BaseLayout(),
+          Obx(
+            () => miniVideoLogic.selectedVideoDetails == null
+                ? const SizedBox()
+                : Padding(
+                    padding: EdgeInsets.only(
+                        bottom: miniVideoLogic.heightOfNavigationBar.value.h),
+                    child: const MiniPlayerVideo(),
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BaseLayout extends StatelessWidget {
+  const _BaseLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     final baseLayoutLogic = Get.find<BaseLayoutLogic>(tag: "1");
     final miniVideoLogic = Get.find<MiniVideoViewLogic>(tag: "1");
 
-    return Obx(() => CustomCupertinoTabScaffold(
-              tabBar: CustomCupertinoTabBar(
-                backgroundColor: baseLayoutLogic.isShortsPageSelected
-                    ? Theme.of(context).focusColor
-                    : Theme.of(context).primaryColor,
-                height: miniVideoLogic.heightOfNavigationBar.value.h,
-                border: Border(
-                    top: BorderSide(
-                        color: baseLayoutLogic.isShortsPageSelected
-                            ? ColorManager(context).grey9
-                            : ColorManager(context).grey1,
-                        width: 1.w)),
-                inactiveColor: baseLayoutLogic.isShortsPageSelected
-                    ? ColorManager(context).white
-                    : ColorManager(context).black,
-                activeColor: baseLayoutLogic.isShortsPageSelected
-                    ? ColorManager(context).white
-                    : ColorManager(context).black,
-                items: [
-                  navigationBarItem(Icons.home_outlined, Icons.home, "Home"),
-                  navigationBarItem(Icons.home_outlined, Icons.home, "Shorts"),
-                  navigationBarItem(Icons.add_circle_outline_outlined,
-                      Icons.add_circle_outlined, "Account"),
-                  navigationBarItem(Icons.subscriptions_outlined,
-                      Icons.subscriptions, "Subscriptions"),
-                  navigationBarItem(Icons.video_library_outlined,
-                      Icons.video_library, "Library"),
-                ],
-              ),
-              controller: baseLayoutLogic.tabController,
-              tabBuilder: (context, index) {
-                return Stack(
-                  children: [
-                    WhichPage(index),
-                    Obx(
-                      () {
-                        return miniVideoLogic.selectedVideoDetails == null
-                            ? const SizedBox()
-                            : const MiniPlayerVideo();
-                      },
-                    ),
-                  ],
-                );
-              },
-            ));
+    return Obx(
+      () => CustomCupertinoTabScaffold(
+        tabBar: CustomCupertinoTabBar(
+          backgroundColor: baseLayoutLogic.isShortsPageSelected
+              ? Theme.of(context).focusColor
+              : Theme.of(context).primaryColor,
+          height: miniVideoLogic.heightOfNavigationBar.value.h,
+          border: Border(
+              top: BorderSide(
+                  color: baseLayoutLogic.isShortsPageSelected
+                      ? ColorManager(context).grey9
+                      : ColorManager(context).grey1,
+                  width: 1.w)),
+          inactiveColor: baseLayoutLogic.isShortsPageSelected
+              ? ColorManager(context).white
+              : ColorManager(context).black,
+          activeColor: baseLayoutLogic.isShortsPageSelected
+              ? ColorManager(context).white
+              : ColorManager(context).black,
+          items: [
+            navigationBarItem(Icons.home_outlined, Icons.home, "Home"),
+            navigationBarItem(Icons.home_outlined, Icons.home, "Shorts"),
+            navigationBarItem(Icons.add_circle_outline_outlined,
+                Icons.add_circle_outlined, "Account"),
+            navigationBarItem(Icons.subscriptions_outlined, Icons.subscriptions,
+                "Subscriptions"),
+            navigationBarItem(
+                Icons.video_library_outlined, Icons.video_library, "Library"),
+          ],
+        ),
+        controller: baseLayoutLogic.tabController,
+        tabBuilder: (context, index) {
+          return WhichPage(index);
+        },
+      ),
+    );
   }
 
   BottomNavigationBarItem navigationBarItem(
