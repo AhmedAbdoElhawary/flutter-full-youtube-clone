d44883ce80cc90d20c83ef704e154b2762c0bea6
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-03 02:17:52 +0200

    return CupertinoTabBar

--- lib/presentation/layouts/base_layout_logic.dart
+++ lib/presentation/layouts/base_layout_logic.dart
@@ -1,23 +1,31 @@
 import 'package:get/get.dart';
+import 'package:youtube/presentation/custom_packages/custom_tab_scaffold/custom_tab_scaffold.dart';
 import 'package:youtube/presentation/pages/shorts/logic/shorts_page_logic.dart';
 
 class BaseLayoutLogic extends GetxController {
+  final tabController = CustomCupertinoTabController();
   final RxBool _isShortsPageSelected = false.obs;
-  final RxInt _selectedPage = 0.obs;
   final _shortsLogic = Get.find<ShortsLogic>(tag: "1");
   final RxBool _isShortsInitialize = false.obs;
+  @override
+  void onInit() {
+    tabControllerListener();
+    super.onInit();
+  }
 
-  int get selectedPage => _selectedPage.value;
-
-  set changeSelectedPage(int index) {
-    _selectedPage.value = index;
-    if (selectedPage == 1) {
-      _isShortsPageSelected.value = true;
-      if (isShortsInitialize) _shortsLogic.stopVideo = false;
-    } else if (isShortsPageSelected) {
-      _isShortsPageSelected.value = false;
-      if (isShortsInitialize) _shortsLogic.stopVideo = true;
-    }
+  tabControllerListener() {
+    tabController.addListener(
+      () {
+        int selectedPage = tabController.index;
+        if (selectedPage == 1) {
+          _isShortsPageSelected.value = true;
+          if (isShortsInitialize) _shortsLogic.stopVideo = false;
+        } else if (isShortsPageSelected) {
+          _isShortsPageSelected.value = false;
+          if (isShortsInitialize) _shortsLogic.stopVideo = true;
+        }
+      },
+    );
   }
 
   bool get isShortsInitialize => _isShortsInitialize.value;
