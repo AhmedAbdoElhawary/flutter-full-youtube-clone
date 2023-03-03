396f11067097bda11109e4f6435e6bf5f4d67be5
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-03 23:00:02 +0200

    refactoring the code

--- lib/core/translations/app_lang.dart
+++ lib/core/translations/app_lang.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:youtube/core/translations/local_storage/local_storage.dart';
-import 'package:youtube/presentation/custom_packages/custom_mini_player/mini_player_logic.dart';
 import 'package:youtube/presentation/layouts/base_layout_logic.dart';
 import 'package:youtube/presentation/pages/home/logic/home_page_logic.dart';
-import 'package:youtube/presentation/pages/shorts/logic/shorts_page_logic.dart';
 
 class AppLanguage extends GetxController {
   String _appLocale = 'en';
@@ -14,8 +12,6 @@ class AppLanguage extends GetxController {
     super.onInit();
     LocalStorage localStorage = LocalStorage();
     Get.put(MiniVideoViewLogic(), tag: "1");
-    Get.put(ShortsLogic(), tag: "1");
-    Get.put(MiniPlayerLogic(), tag: "1");
     Get.put(BaseLayoutLogic(), tag: "1");
 
     _appLocale = await localStorage.languageSelected ?? 'en';
