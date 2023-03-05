a6eea50a3841cf1b8859fd2a79ebc7b4accc11b1
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-06 00:40:25 +0200

    add getMyPlayLists in ChannelPlayListAPIs

--- lib/data/data_sources/remote/api/channel/channel_playlist/channel_playlist_apis.dart
+++ lib/data/data_sources/remote/api/channel/channel_playlist/channel_playlist_apis.dart
@@ -10,15 +10,22 @@ part 'channel_playlist_apis.g.dart';
 abstract class ChannelPlayListAPIs {
   factory ChannelPlayListAPIs(Dio dio, {String baseUrl}) = _ChannelPlayListAPIs;
 
-  @GET("playlists?part=snippet%2CcontentDetails&maxResults=20")
+  @GET("playlists?part=snippet,contentDetails,status&maxResults=50")
   Future<PlayLists> getChannelPlayLists({
     @Query("key") final String apiKey = apiKey,
     @Query("channelId") required String channelId,
   });
 
-  @GET("playlistItems?part=contentDetails&maxResults=20")
+  @GET("playlists?part=snippet,contentDetails,status&maxResults=50&mine=true")
+  Future<PlayLists> getMyPlayLists({
+    @Query("key") final String apiKey = apiKey,
+    @Query("access_token") required String accessToken,
+  });
+
+  @GET("playlistItems?part=contentDetails&maxResults=50")
   Future<PlayListVideos> getChannelPlayListItemsIds({
     @Query("key") final String apiKey = apiKey,
     @Query("playlistId") required String playlistId,
+    @Query("access_token") required String accessToken,
   });
 }
