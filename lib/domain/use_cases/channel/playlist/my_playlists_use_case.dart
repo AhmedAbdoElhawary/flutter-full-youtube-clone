ef1f9ac69e6083ac960f49a9cd47ba263a8f9ea3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-06 00:37:43 +0200

    create MyPlaylistsUseCase

---
+++ lib/domain/use_cases/channel/playlist/my_playlists_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:youtube/core/functions/handling_errors/api_result.dart';
+import 'package:youtube/core/use_case/use_case.dart';
+import 'package:youtube/data/models/channel_details/playlists/playlists.dart';
+import 'package:youtube/domain/repositories/channel/channel_playlist_repository.dart';
+
+class MyPlaylistsUseCase implements FutureUseCase<ApiResult<PlayLists>, void> {
+  final ChannelPlayListDetailsRepository _listDetailsRepository;
+
+  MyPlaylistsUseCase(this._listDetailsRepository);
+
+  @override
+  Future<ApiResult<PlayLists>> call({required void params}) {
+    return _listDetailsRepository.getMyPlayLists();
+  }
+}
