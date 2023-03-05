767dd6d097cd9732282373611eedd2d04267447a
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-06 00:41:36 +0200

    refactoring the code

--- lib/domain/repositories/channel/channel_playlist_repository.dart
+++ lib/domain/repositories/channel/channel_playlist_repository.dart
@@ -4,10 +4,11 @@ import 'package:youtube/data/models/videos_details/videos_details.dart';
 
 abstract class ChannelPlayListDetailsRepository {
   Future<void> clearChannelPlayLists({required String channelId});
+  Future<void> clearMyPlayLists();
 
   Future<ApiResult<PlayLists>> getChannelPlayLists({required String channelId});
+  Future<ApiResult<PlayLists>> getMyPlayLists();
 
-  Future<ApiResult<List<VideosDetails>>> getChannelPlayListItem({
-    required String playlistId,
-  });
+  Future<ApiResult<List<VideosDetails>>> getChannelPlayListItem(
+      {required String playlistId});
 }
