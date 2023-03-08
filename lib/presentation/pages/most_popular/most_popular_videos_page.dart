14a6da3d767958191adeb19d37f2f4724b6e389a
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-08 23:42:00 +0200

    change the height to size in SvgIcon

--- lib/presentation/pages/most_popular/most_popular_videos_page.dart
+++ lib/presentation/pages/most_popular/most_popular_videos_page.dart
@@ -56,7 +56,7 @@ class _MostPopularVideosPageState extends State<MostPopularVideosPage> {
                       RSizedBox(width: 20),
                       SearchIcon(),
                       RSizedBox(width: 20),
-                      SvgIcon(IconsAssets.menuPointsVerticalIcon, height: 15),
+                      SvgIcon(IconsAssets.menuPointsVerticalIcon, size: 15),
                       RSizedBox(width: 20),
                     ],
                   ),
