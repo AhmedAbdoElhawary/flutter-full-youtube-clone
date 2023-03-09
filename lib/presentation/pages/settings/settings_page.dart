c5dabaf0b9b689adfbb6c3d1669e013dfd41e6d3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-09 02:27:46 +0200

    create SettingsPage

---
+++ lib/presentation/pages/settings/settings_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:youtube/core/resources/color_manager.dart';
+import 'package:youtube/core/resources/styles_manager.dart';
+import 'package:youtube/presentation/common_widgets/app_bars/custom_app_bar.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          CustomAppBar.normalAppBar(context, "Settings", withoutIcons: true),
+      body: ListView.separated(
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              // Go(context).to(page);
+            },
+            child: Text(
+                  "General",
+                  style: getNormalStyle(
+                      color: ColorManager(context).black, fontSize: 15),
+                ),
+          ),
+          separatorBuilder: (context, index) => const RSizedBox(height: 20),
+          itemCount: 5),
+    );
+  }
+}
