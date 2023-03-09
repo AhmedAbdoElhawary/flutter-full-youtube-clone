400741b9f0a2c2ef04d678874d058abe215851f0
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-09 02:29:01 +0200

    add getMyChannelInfo in CacheChannelAPIs

--- lib/data/data_sources/local/channnel/interfaces/cache_channel_apis.dart
+++ lib/data/data_sources/local/channnel/interfaces/cache_channel_apis.dart
@@ -1,3 +1,5 @@
+import 'package:youtube/data/models/channel_details/channel_details.dart';
+
 import '../../../../../domain/entities/cache_items/cached_item.dart';
 import '../../../../models/channel_details/my_subscriptions/my_subscriptions_details.dart';
 
@@ -7,4 +9,8 @@ abstract class CacheChannelAPIs {
   Future<MySubscriptionsDetails?> getMySubscriptionsChannels();
   Future<void> saveMySubscriptionsChannels(MySubscriptionsDetails subscriptions);
   Future<void> clearMySubscriptionsChannels();
+
+  Future<ChannelSubDetails?> getMyChannelInfo();
+  Future<void> saveMyChannelInfo(ChannelSubDetails details);
+  Future<void> clearMyChannelInfo();
 }
