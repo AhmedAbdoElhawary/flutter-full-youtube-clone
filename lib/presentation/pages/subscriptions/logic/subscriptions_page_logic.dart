fc30fc3393bcf8aaa89081527cb985e6c8998cb3
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-14 06:17:38 +0200

    change Obx to GetBuilder

--- lib/presentation/pages/subscriptions/logic/subscriptions_page_logic.dart
+++ lib/presentation/pages/subscriptions/logic/subscriptions_page_logic.dart
@@ -2,16 +2,25 @@ import 'package:get/get.dart';
 import 'package:youtube/data/models/channel_details/my_subscriptions/my_subscriptions_details.dart';
 
 class SubscriptionsPageLogic extends GetxController {
-  final Rx<int?> selectedChannelIndex = Rxn<int?>();
+  final Rx<int?> _selectedChannelIndex = Rxn<int?>();
   final Rx<MySubscriptionsDetails?> _allSubscribedChannels =
       Rxn<MySubscriptionsDetails?>();
   final Rx<MySubscriptionsItem?> _selectedChannelItemDetails =
       Rxn<MySubscriptionsItem?>();
 
-  MySubscriptionsDetails? get allSubscribedChannels => _allSubscribedChannels.value;
+  int? get selectedChannelIndex => _selectedChannelIndex.value;
+
+  set selectedChannelIndex(int? value) {
+    _selectedChannelIndex.value = value;
+    update(["update-selected-channel"]);
+  }
+
+  MySubscriptionsDetails? get allSubscribedChannels =>
+      _allSubscribedChannels.value;
 
   set allSubscribedChannels(MySubscriptionsDetails? value) {
     _allSubscribedChannels.value = value;
+    update(["update-subscribed-channels"]);
   }
 
   MySubscriptionsItem? get selectedChannelItemDetails =>
@@ -19,5 +28,6 @@ class SubscriptionsPageLogic extends GetxController {
 
   set selectedChannelItemDetails(MySubscriptionsItem? value) {
     _selectedChannelItemDetails.value = value;
+    update(["update-selected-channel"]);
   }
 }
