61134e59e3562bee9e1c51bfc5aaf64a40f0c53b
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-04-08 15:59:07 +0200

    remove refresh indicator

--- lib/presentation/pages/home/views/home_page.dart
+++ lib/presentation/pages/home/views/home_page.dart
@@ -40,7 +40,6 @@ class _HomePageState extends State<HomePage> {
             ),
             _VideosList(index),
           ],
-          onRefreshData: () async {},
           physics: const BouncingScrollPhysics(),
           initialInViewIds: const ['0'],
           isInViewPortCondition:
