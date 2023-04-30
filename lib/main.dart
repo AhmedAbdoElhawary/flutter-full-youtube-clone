021c7e5d43bc66054176f83d2be9a80ee986b8c3
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-04-30 14:23:46 -0700

    remove googleSignIn in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:youtube/presentation/my_app.dart';
 import 'package:youtube/core/utility/injector.dart';
-import 'package:youtube/core/utility/private_key.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await initializeDependencies();
-  final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-  final googleAuth = await googleUser?.authentication;
-  String? token = googleAuth?.accessToken ?? "";
-  accessToken = token;
   runApp(const MyApp());
 }
-
-final GoogleSignIn googleSignIn =
-    GoogleSignIn(scopes: ['https://www.googleapis.com/auth/youtube.force-ssl']);
