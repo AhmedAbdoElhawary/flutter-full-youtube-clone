5e0fdd0ae15bcdc8df3b156d21013b301c84891a
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-05-17 13:47:08 -0700

    add const

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:youtube/presentation/my_app.dart';
-import 'package:youtube/core/utility/injector.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init();
-  await initializeDependencies();
   runApp(const MyApp());
 }
