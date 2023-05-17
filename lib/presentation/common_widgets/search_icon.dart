5445bb1b34f24d56212544e1aff8aac8d75c3a54
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-05-17 13:45:49 -0700

    change functions to helpers

--- lib/presentation/common_widgets/search_icon.dart
+++ lib/presentation/common_widgets/search_icon.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:youtube/config/routes/route_app.dart';
 import 'package:youtube/core/resources/assets_manager.dart';
 import 'package:youtube/core/resources/color_manager.dart';
+import 'package:youtube/core/utility/constants.dart';
 import 'package:youtube/presentation/common_widgets/svg_icon.dart';
 import 'package:youtube/presentation/pages/search/search_page.dart';
 
@@ -12,7 +13,7 @@ class SearchIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Go(context).to(const SearchPage());
+        Go(savedContext!).to(const SearchPage());
       },
       child: SvgIcon(
         IconsAssets.searchIcon,
