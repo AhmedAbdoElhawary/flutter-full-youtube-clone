cd40cbfe0af9383ffd4efb5ffc88c73ed226da81
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-05-21 11:06:55 -0700

    handle ArrowBack size

--- lib/presentation/common_widgets/arrow_back.dart
+++ lib/presentation/common_widgets/arrow_back.dart
@@ -5,10 +5,15 @@ import 'package:youtube/core/resources/color_manager.dart';
 import 'package:youtube/presentation/common_widgets/svg_icon.dart';
 
 class ArrowBack extends StatelessWidget {
-  const ArrowBack({this.makeItWhite = false, this.onTapBack, Key? key})
+  const ArrowBack(
+      {this.makeItWhite = false,
+      this.makeItSmall = false,
+      this.onTapBack,
+      Key? key})
       : super(key: key);
   final VoidCallback? onTapBack;
   final bool makeItWhite;
+  final bool makeItSmall;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -19,7 +24,8 @@ class ArrowBack extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsetsDirectional.only(top: 19, bottom: 19),
         child: SvgIcon(IconsAssets.arrowLeftIcon,
-            color: makeItWhite ? BaseColorManager.white : null),
+            color: makeItWhite ? BaseColorManager.white : null,
+            size: makeItSmall ? 18 : 25),
       ),
     );
   }
