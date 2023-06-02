2aee8464fa62eaaf432d55d269e80bc43a9b7219
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-06-02 11:51:13 -0700

    rebuild AuthService

--- lib/core/helpers/auth_service.dart
+++ lib/core/helpers/auth_service.dart
@@ -1,35 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:youtube/core/utility/private_key.dart';
 
 class AuthService {
+  static final GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: ['https://www.googleapis.com/auth/youtube.force-ssl']);
+
   static signInWithGoogle() async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
-
-    final GoogleSignIn googleSignIn = GoogleSignIn(
-        scopes: ['https://www.googleapis.com/auth/youtube.force-ssl']);
-
-    final GoogleSignInAccount? googleSignInAccount =
-        await googleSignIn.signIn();
-
-    if (googleSignInAccount != null) {
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount.authentication;
-
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-      );
-
-      accessToken = credential.accessToken ?? "";
-
-      final UserCredential userCredential =
-          await auth.signInWithCredential(credential);
-
-      user = userCredential.user;
-    }
-
-    return user;
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    final googleAuth = await googleUser?.authentication;
+    String? token = googleAuth?.accessToken ?? "";
+    accessToken = token;
   }
 }
