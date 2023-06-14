259ce4ea9c2232fc4501e332b7e2bcd9621e34ff
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-06-14 14:38:44 -0700

    reformat the code

--- lib/presentation/pages/channel_profile/tab_bar_views/videos_horizontal_descriptions_list.dart
+++ lib/presentation/pages/channel_profile/tab_bar_views/videos_horizontal_descriptions_list.dart
@@ -45,7 +45,7 @@ class VideoHorizontalDescriptionsList extends StatelessWidget {
                 Text(
                   "${videoDetails.getVideoViewsCount()} views . ${videoDetails.getVideoPublishedTime()} ago",
                   style: getNormalStyle(
-                      color:  BaseColorManager.grey, fontSize: 12),
+                      color: BaseColorManager.grey, fontSize: 12),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
