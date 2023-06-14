892d6cfee0af0f87a243976be413628faa172e1e
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-06-14 14:40:17 -0700

    reformat the code

--- lib/domain/use_cases/channel/channel_details/delete_subscription_use_case.dart
+++ lib/domain/use_cases/channel/channel_details/delete_subscription_use_case.dart
@@ -2,17 +2,21 @@ import 'package:youtube/core/helpers/handling_errors/api_result.dart';
 import 'package:youtube/core/use_case/use_case.dart';
 import 'package:youtube/domain/repositories/channel/channel_details_repository.dart';
 
-class DeleteSubscriptionUseCase implements FutureUseCase<ApiResult<void>, DeleteSubscriptionUseCaseParameter> {
+class DeleteSubscriptionUseCase
+    implements
+        FutureUseCase<ApiResult<void>, DeleteSubscriptionUseCaseParameter> {
   final ChannelDetailsRepository _channelDetailsRepository;
 
   DeleteSubscriptionUseCase(this._channelDetailsRepository);
 
   @override
-  Future<ApiResult<void>> call({required DeleteSubscriptionUseCaseParameter params}) {
+  Future<ApiResult<void>> call(
+      {required DeleteSubscriptionUseCaseParameter params}) {
     return _channelDetailsRepository.deleteSubscription(params.subscriptionId);
   }
 }
-class DeleteSubscriptionUseCaseParameter{
+
+class DeleteSubscriptionUseCaseParameter {
   String subscriptionId;
 
   DeleteSubscriptionUseCaseParameter({required this.subscriptionId});
