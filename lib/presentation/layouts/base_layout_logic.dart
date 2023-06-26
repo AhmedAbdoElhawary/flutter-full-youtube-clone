374ef80f260e1142131d5ef429c0c96647f0b9b1
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-06-26 03:52:02 -0700

    add _pagesRoutesPool in BaseLayoutLogic

--- lib/presentation/layouts/base_layout_logic.dart
+++ lib/presentation/layouts/base_layout_logic.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:youtube/presentation/pages/home/logic/home_page_logic.dart';
+import 'package:youtube/presentation/pages/home/views/home_page.dart';
+import 'package:youtube/presentation/pages/library/library_page.dart';
 import 'package:youtube/presentation/pages/shorts/logic/shorts_page_logic.dart';
+import 'package:youtube/presentation/pages/shorts/shorts_page.dart';
+import 'package:youtube/presentation/pages/subscriptions/subscriptions_page.dart';
 
 class BaseLayoutLogic extends GetxController {
   final RxBool _isShortsPageSelected = false.obs;
   final RxBool _isShortsInitialize = false.obs;
   final RxInt _selectedPage = 0.obs;
   static late final TabController tabController;
+  final Map<String, List<Widget>> _pagesRoutesPool = {
+    "home": [const HomePage()],
+    "short": [const ShortsPage()],
+    "subscription": [const SubscriptionsPage()],
+    "library": [const LibraryPage()],
+  };
+
+
 
   tabControllerListener() {
     if (getSelectedPage != 0) {
@@ -46,6 +58,12 @@ class BaseLayoutLogic extends GetxController {
     _isShortsPageSelected.value = value;
     update(["update-selected-page"]);
   }
+  //
+  // Map<String, List<String>> get getLastHomeRoute => _pagesRoutesPool;
+  //
+  // set pagesRoutesPool(Map<String, List<String>> value) {
+  //   _pagesRoutesPool = value;
+  // }
 
   bool get getShortsInitialize => _isShortsInitialize.value;
 
